506097358a61508dad33ef2d79fc4d5a126e0cd5
n1nj4sec <contact@n1nj4.eu>
2015-09-22 22:19:21 +0200

    adding kill command

---
+++ pupy/modules/process_kill.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF8 -*-
+from pupylib.PupyModule import *
+
+__class_name__="KillModule"
+
+class KillModule(PupyModule):
+	""" kill a process """
+
+	def init_argparse(self):
+		self.arg_parser = PupyArgumentParser(prog="kill", description=self.__doc__)
+		self.arg_parser.add_argument('pid', type=int, help='pid to kill')
+
+	def run(self, args):
+		self.client.conn.modules.os.kill(args.pid,9)
+		self.success("process killed !")
+
