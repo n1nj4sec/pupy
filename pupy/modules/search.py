629796613adfd6930d91498b5307ed5f0c5fa620
n1nj4sec <contact@n1nj4.eu>
2015-10-03 15:13:16 +0200

    fixing jobs && jobs now exists properly on the client when the server exits

--- pupy/modules/search.py
+++ pupy/modules/search.py
@@ -5,16 +5,21 @@ __class_name__="SearchModule"
 
 class SearchModule(PupyModule):
 	""" walk through a directory and recursively search a string into files """
-
+	daemon=True
 	def init_argparse(self):
 		self.arg_parser = PupyArgumentParser(prog="search", description=self.__doc__)
 		self.arg_parser.add_argument('path', help='path')
-		self.arg_parser.add_argument('strings', nargs='+',metavar='string',  help='strings to search')
+		self.arg_parser.add_argument('-e','--extensions',metavar='ext1,ext2,...', help='limit to some extensions')
+		self.arg_parser.add_argument('strings', nargs='+', metavar='string', help='strings to search')
+		self.arg_parser.add_argument('-m','--max-size', type=int, default=None, help='max file size')
 
 	def run(self, args):
-		self.client.load_package("pupyutils.search")
+		self.client.load_package("pupyutils.search", force=True)
+		exts=[]
+		if args.extensions:
+			exts=args.extensions.split(',')
 		self.info("searching strings %s in %s ..."%(args.strings, args.path))
-		for res in self.client.conn.modules['pupyutils.search'].search_path(args.path, args.strings):
+		for res in self.client.conn.modules['pupyutils.search'].search_path(args.path, args.strings, files_extensions=exts, max_size=args.max_size):
 			self.success("%s:%s > %s"%(res[0],res[1],res[2]))
 		self.info("search finished !")
 
