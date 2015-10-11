ee066654a93301895a746956b1fb5ae2e75d3fac
n1nj4sec <contact@n1nj4.eu>
2015-10-11 16:37:53 +0200

    adding arch checking to memory_exec issue #40

---
+++ pupy/pupylib/utils/pe.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: UTF8 -*-
+
+import pefile
+
+def get_pe_arch(*args, **kwargs):
+	pe=None
+	if args:
+		pe = pefile.PE(args[0], fast_load=True)
+	elif "data" in kwargs:
+		pe = pefile.PE(data=kwargs["data"], fast_load=True)
+	else:
+		raise NameError("at least a path or data must be supplied to get_arch")
+	if pe.OPTIONAL_HEADER.Magic==0x010b:
+		return "32bit"
+	elif pe.OPTIONAL_HEADER.Magic==0x020b:
+		return "64bit"
+	else:
+		return "UNKNOWN"
+
