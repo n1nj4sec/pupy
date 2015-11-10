2fd5d1c7e1bc6fcef1ec977c258bc2ef17f9893a
n1nj4sec <contact@n1nj4.eu>
2015-11-10 20:27:13 +0100

    missing license headers

--- pupy/modules/pyshell.py
+++ pupy/modules/pyshell.py
@@ -1,4 +1,6 @@
 # -*- coding: UTF8 -*-
+# Copyright (c) 2015, Nicolas VERDIER (contact@n1nj4.eu)
+# Pupy is under the BSD 3-Clause license. see the LICENSE file at the root of the project for the detailed licence terms
 from pupylib.PupyModule import *
 import sys
 import subprocess
