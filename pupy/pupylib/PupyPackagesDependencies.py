a73c7fb0d4884ed4aaa7dd6a1d05fd0ad6ec0367
n1nj4sec <contact@n1nj4.eu>
2016-01-08 23:02:01 +0100

    adding missing licence headers

--- pupy/pupylib/PupyPackagesDependencies.py
+++ pupy/pupylib/PupyPackagesDependencies.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF8 -*-
+# Copyright (c) 2015, Nicolas VERDIER (contact@n1nj4.eu)
+# Pupy is under the BSD 3-Clause license. see the LICENSE file at the root of the project for the detailed licence terms
 
 LOAD_PACKAGE=1
 LOAD_DLL=2
