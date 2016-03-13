971591da1b482f1b887df5b887e9ec210c3c798a
n1nj4sec <contact@n1nj4.eu>
2016-03-13 13:47:51 -0400

    pupygen ip detection fix

--- pupy/pupylib/utils/network.py
+++ pupy/pupylib/utils/network.py
@@ -1,9 +1,10 @@
 # -*- coding: UTF8 -*-
 import subprocess
+import re
 
 def get_local_ip(iface = 'eth0'):
 	try:
-		return subprocess.check_output(["ifconfig", iface]).split("\n")[1].split()[1][5:]
+		return re.findall("[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", subprocess.check_output(["ifconfig", iface]).split("\n")[1])[0]
 		#TODO same for windows
 	except Exception:
 		return None
