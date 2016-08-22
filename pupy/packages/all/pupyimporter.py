43e041b044155206aff81183c5b2ed36477a6d6f
Oleksii Shevchuk <alxchk@gmail.com>
2016-08-22 15:10:00 +0300

    Add trivial support for loading shared objects on linux

--- pupy/packages/all/pupyimporter.py
+++ pupy/packages/all/pupyimporter.py
@@ -1,28 +1,31 @@
+# -*- coding: utf-8 -*-
 # ---------------------------------------------------------------
 # Copyright (c) 2015, Nicolas VERDIER (contact@n1nj4.eu)
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-# 
+#
 # 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-# 
+#
 # 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-# 
+#
 # 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
 # ---------------------------------------------------------------
 # This module uses the builtins modules pupy and _memimporter to load python modules and packages from memory, including .pyd files (windows only)
 # Pupy can dynamically add new modules to the modules dictionary to allow remote importing of python modules from memory !
 #
 import sys, imp, zlib, marshal
+import tempfile
+
 builtin_memimporter=False
 try:
     import _memimporter
     builtin_memimporter=True
 except ImportError:
     pass
-    
+
 modules={}
 try:
     import pupy
@@ -102,6 +105,12 @@ class PupyPackageLoader:
                 mod.__loader__ = self
                 mod.__package__ = fullname.rsplit('.',1)[0]
                 sys.modules[fullname]=mod
+            elif self.extension in ('so'):
+                with tempfile.NamedTemporaryFile(mode='wb') as tmpfile:
+                    tmpfile.write(self.contents)
+                    tmpfile.flush()
+                    initname = fullname.rsplit(".",1)[-1]
+                    sys.modules[fullname]=imp.load_dynamic(initname, tmpfile.name)
         except Exception as e:
             if fullname in sys.modules:
                 del sys.modules[fullname]
@@ -175,5 +184,3 @@ def load_pywintypes():
 def install():
     sys.meta_path.append(PupyPackageFinder(modules))
     sys.path_importer_cache.clear()
-
-
