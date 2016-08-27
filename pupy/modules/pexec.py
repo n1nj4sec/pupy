cf0a244cccdee1dc42f7ec6c6f5b8296908abac8
Oleksii Shevchuk <alxchk@gmail.com>
2016-08-27 20:53:18 +0300

    Make upload/download/pexec commands more smart

--- pupy/modules/pexec.py
+++ pupy/modules/pexec.py
@@ -8,6 +8,12 @@ import subprocess
 import time
 import datetime
 import os
+import re
+import stat
+import pupygen
+import tempfile
+
+from rpyc.utils.classic import upload
 
 __class_name__="PExec"
 
@@ -19,6 +25,7 @@ class PExec(PupyModule):
     pipe = None
     completed = False
     terminate = False
+    updl = re.compile('\^([^\^]+)\^([<>])([^\^]+)\^')
     # daemon = True
 
     dependencies = [ "pupyutils.safepopen" ]
@@ -48,7 +55,8 @@ class PExec(PupyModule):
         self.arg_parser.add_argument(
             'arguments',
             nargs=argparse.REMAINDER,
-            help='CMD args'
+            help='CMD args. You can use ^/local/path^[>|<]/remote/path^ '
+            'form to upload|download files before|after command'
         )
 
     def run(self, args):
@@ -56,22 +64,102 @@ class PExec(PupyModule):
             self.error('No command specified {}'.format(args.__dict__))
             return
 
-        rsubprocess = self.client.conn.modules['subprocess']
         cmdargs = args.arguments
 
-        if args.s:
-            cmdargs = [
-                'cmd.exe', '/c',
-            ] + cmdargs if self.client.is_windows() else [
-                '/bin/sh', '-c', ' '.join(
-                    '"'+x.replace('"','\"')+'"' for x in cmdargs
-                )
-            ]
+        to_upload = []
+        to_download = []
+        to_delete = []
+
+        rsubprocess = self.client.conn.modules['subprocess']
+        ros = self.client.conn.modules['os']
+
+        for i, arg in enumerate(cmdargs):
+            for local, direction, remote in self.updl.findall(arg):
+                if local == '$SELF$':
+                    platform = self.client.platform()
+                    if not platform in ('windows', 'linux'):
+                        self.error('Couldn\'t use $SELF$ on platform {}'.format(platform))
+                    xlocal = '$SELF$'
+                else:
+                    xlocal = os.path.expandvars(local)
+
+                xremote = ros.path.expandvars(remote)
+
+                if direction == '<':
+                    to_download.append((xremote, xlocal))
+                else:
+                    if xlocal == '$SELF$':
+                        mode = 0711
+                        to_upload.append((xlocal, xremote, mode))
+                    else:
+                        if not os.path.exists(xlocal):
+                            self.error('No local file {} found (scheduled for upload)'.format(
+                                xlocal))
+
+                        mode = os.stat(xlocal).st_mode
+                        to_upload.append((xlocal, xremote, mode))
+
+                arg = arg.replace('^'+local+'^'+direction+remote+'^', remote)
+
+            cmdargs[i] = arg
+
+        for local, remote, mode in to_upload:
+            if local == '$SELF$':
+                platform = self.client.platform()
+                arch = ''
+                config = self.client.get_conf()
+
+                payload = b''
+                if self.client.is_proc_arch_64_bits():
+                    if platform == 'windows':
+                        payload = pupygen.get_edit_pupyx64_exe(config)
+                    else:
+                        payload = pupygen.get_edit_pupyx64_lin(config)
+
+                    arch = 'x64'
+                else:
+                    if platform == 'windows':
+                        payload = pupygen.get_edit_pupyx86_exe(config)
+                    else:
+                        payload = pupygen.get_edit_pupyx86_lin(config)
+
+                    arch = 'x86'
+
+                with tempfile.NamedTemporaryFile() as tmp:
+                    self.info('Store pupy/{}/{}/size={} to {}'.format(
+                        platform, arch, len(payload), tmp.name))
+                    tmp.write(payload)
+                    tmp.flush()
+                    self.info('Upload {} -> {}'.format(tmp.name, remote))
+                    upload(self.client.conn, tmp.name, remote)
+            else:
+                self.info('Upload {} -> {}'.format(local, remote))
+                upload(self.client.conn, local, remote)
+
+            ros.chmod(remote, mode)
 
         cmdenv = {
             'stderr': (None if args.n else subprocess.STDOUT),
             'universal_newlines': False,
         }
+
+        if len(cmdargs) == 1 and ' ' in cmdargs[0]:
+            cmdenv.update({
+                'shell': True
+            })
+            cmdargs = cmdargs[0]
+        else:
+            cmdenv.update({
+                'shell': False
+            })
+            if args.s:
+                cmdargs = [
+                    'cmd.exe', '/c',
+                ] + cmdargs if self.client.is_windows() else [
+                    '/bin/sh', '-c', ' '.join(
+                        '"'+x.replace('"','\"')+'"' for x in cmdargs
+                    )
+                ]
 
         if self.client.is_windows():
             if not args.F:
@@ -90,11 +178,13 @@ class PExec(PupyModule):
 
         rdatetime = self.client.conn.modules['datetime']
 
-        self.success('Started at (local:{} / remote:{}): '.format(
-            datetime.datetime.now(), rdatetime.datetime.now()))
+        if hasattr(self.job, 'id'):
+            self.success('Started at (local:{} / remote:{}): '.format(
+                datetime.datetime.now(), rdatetime.datetime.now()))
+
         self.success('Command: {}'.format(' '.join(
             x if not ' ' in x else "'" + x + "'" for x in cmdargs
-        )))
+        ) if not cmdenv['shell'] else cmdargs))
 
         log = None
         if args.log:
@@ -136,13 +226,19 @@ class PExec(PupyModule):
             log.close()
 
         if self.pipe.returncode == 0:
-            self.success('Finished at (local:{} / remote:{}): '.format(
+            self.success('Successful at (local:{} / remote:{}): '.format(
                 datetime.datetime.now(), rdatetime.datetime.now()))
         else:
-            self.error('Finished at (local:{} / remote:{})'.format(
-                datetime.datetime.now(), rdatetime.datetime.now(),
+            self.error('Ret: {} at (local:{} / remote:{})'.format(
+                self.pipe.returncode, datetime.datetime.now(), rdatetime.datetime.now(),
                 ))
-            self.error('Ret: {}'.format(self.pipe.returncode))
+
+        for remote, local in to_download:
+            if ros.path.exists(remote):
+                self.info('Download {} -> {}'.format(remote, local))
+                download(self.client.conn, remote, local)
+            else:
+                self.error('Remote file {} not exists (scheduled for download)'.format(remote))
 
         if hasattr(self.job, 'id'):
             self.job.pupsrv.handler.display_srvinfo('(Job id: {}) Command {} completed'.format(
--- pupy/modules/upload.py
+++ pupy/modules/upload.py
@@ -13,10 +13,43 @@ class UploaderScript(PupyModule):
     def init_argparse(self):
         self.arg_parser = PupyArgumentParser(prog='upload', description=self.__doc__)
         self.arg_parser.add_argument('local_file', metavar='<local_path>', completer=path_completer)
-        self.arg_parser.add_argument('remote_file', metavar='<remote_path>')
+        self.arg_parser.add_argument('remote_file', nargs='?', metavar='<remote_path>')
+
     def run(self, args):
-        dst = self.client.conn.modules['os.path'].expandvars(args.remote_file)
-        if dst.endswith('.'):
-            dst = dst.replace('.', args.local_file.split(os.sep)[-1])
-        upload(self.client.conn, args.local_file, dst)
-        self.success("file local:%s uploaded to remote:%s"%(args.local_file, dst))
+        ros = self.client.conn.modules['os']
+        localfile =  os.path.expandvars(args.local_file)
+
+        if args.remote_file:
+            remotefile = ros.path.expandvars(args.remote_file)
+        else:
+            ros = self.client.conn.modules['os']
+            rtempfile = self.client.conn.modules['tempfile']
+            tempdir = rtempfile.gettempdir()
+            remotefile = ros.path.join(tempdir, os.path.basename(localfile))
+
+        if remotefile.endswith('.'):
+            remotefile = remotefile('.', args.local_file.split(os.sep)[-1])
+
+        if os.path.isfile(localfile) and ros.path.isdir(remotefile):
+            remotefile = ros.path.join(remotefile, os.path.basename(localfile))
+
+        self.info(
+            "Uploading local:%s to remote:%s (size=%d)"%(
+                localfile,
+                remotefile,
+                os.stat(localfile).st_size
+            )
+        )
+
+        upload(
+            self.client.conn,
+            localfile,
+            remotefile
+        )
+
+        self.success("file local:%s uploaded to remote:%s"%(localfile, remotefile))
+
+        self.client.conn.modules['os'].chmod(
+            remotefile,
+            os.stat(localfile).st_mode
+        )
