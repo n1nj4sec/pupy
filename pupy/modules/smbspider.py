a688d443dc6402a154eacc06d36440bbd755c24a
Alessandro ZANNI <alessandro.zanni@bt.com>
2016-09-15 12:12:35 +0200

    cleaning code + adding extention option

--- pupy/modules/smbspider.py
+++ pupy/modules/smbspider.py
@@ -2,15 +2,6 @@
 # Code modified from the awesome tool CrackMapExec: /cme/spider/smbspider.py
 # Thank you to byt3bl33d3r for its work
 from pupylib.PupyModule import *
-from pupylib.utils.rpyc_utils import redirected_stdo
-import pupygen
-import re
-import os
-import tempfile
-import random
-import string
-from rpyc.utils.classic import upload
-from packages.all.smbmap import connect
 from netaddr import *
 
 __class_name__="SMBSpider"
@@ -25,7 +16,7 @@ class SMBSpider(PupyModule):
         
         example = 'Examples:\n'
         example += '>> run smbspider --pattern password --content 192.168.0.1\n'
-        example += '>> run smbspider -u john -p password1 -d TEST --regex password.* pwd.* --content 192.168.0.1\n'
+        example += '>> run smbspider -u john -p password1 -d TEST --regex password.* pwd.* --content -e txt,ini 192.168.0.1\n'
         example += '>> run smbspider -u john --regex password.* -H \'aad3b435b51404eeaad3b435b51404ee:da76f2c4c96028b7a6111aef4a50a94d\' 172.16.0.20\n'
 
         self.arg_parser = PupyArgumentParser(prog="smbspider", description=self.__doc__, epilog=example)
@@ -43,10 +34,14 @@ class SMBSpider(PupyModule):
         sgroup.add_argument("--exclude-dirs", type=str, metavar='DIR_LIST', default='', help='Directories to exclude from spidering')
         sgroup.add_argument("--pattern", nargs='*', help='Pattern(s) to search for in folders, filenames and file content')
         sgroup.add_argument("--regex", nargs='*', help='Regex(s) to search for in folders, filenames and file content')
+        sgroup.add_argument('-e','--extensions',metavar='ext1,ext2,...', help='limit to some extensions')
         sgroup.add_argument("--depth", type=int, default=10, help='Spider recursion depth (default: 10)')
         sgroup.add_argument('-m','--max-size', type=int, default=7000000, help='max file size in byte (default 7 Mo)')
 
     def run(self, args):
+        exts=[]
+        if args.extensions:
+            exts=args.extensions.split(',')
 
         if "/" in args.target[0]:
             hosts = IPNetwork(args.target[0])
@@ -56,12 +51,11 @@ class SMBSpider(PupyModule):
 
         self.info("Loading dependencies")
         self.client.load_package("impacket")
-        self.client.load_package("calendar")
-        self.client.load_package("smbspider")
+        self.client.load_package("pupyutils.smbspider")
 
         for host in hosts:
             self.info("Connecting to the remote host: %s:%s" % (host, str(args.port)))
-            smbspider = self.client.conn.modules["smbspider"].SMBSpider(host, args.domain, args.port, args.user, args.passwd, args.hash, args.content, args.regex, args.share, args.exclude_dirs, args.pattern, args.max_size)
+            smbspider = self.client.conn.modules["pupyutils.smbspider"].SMBSpider(host, args.domain, args.port, args.user, args.passwd, args.hash, args.content, args.regex, args.share, args.exclude_dirs, exts, args.pattern, args.max_size)
             logged = smbspider.login()
             if not logged:
                 self.error("Connection failed !")
@@ -70,6 +64,7 @@ class SMBSpider(PupyModule):
             # spider all shares
             if args.share == 'all':
                 for share in smbspider.list_share():
+                    # self.info("Spidering remote share smb://%s/%s" % (host, share))
                     smbspider.set_share(share)
                     for res in smbspider.spider(args.spider, int(args.depth)):
                         self.success("%s" % res)
