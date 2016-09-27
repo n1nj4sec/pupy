13c7eb39b036bded5360bef7d8f538d9665a21f1
n1nj4sec <contact@n1nj4.eu>
2016-09-27 21:21:49 +0200

    removing unused dependency

--- pupy/modules/smbspider.py
+++ pupy/modules/smbspider.py
@@ -2,7 +2,7 @@
 # Code modified from the awesome tool CrackMapExec: /cme/spider/smbspider.py
 # Thank you to byt3bl33d3r for its work
 from pupylib.PupyModule import *
-from netaddr import *
+#from netaddr import *
 
 __class_name__="SMBSpider"
 
