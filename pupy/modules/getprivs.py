6dd52fbbe7e622a811e7f871b499d13c3464389d
quentinhardy <quentin.hardy@protonmail.com>
2016-10-04 08:38:44 -0400

    New option for getting current process privileges

--- pupy/modules/getprivs.py
+++ pupy/modules/getprivs.py
@@ -1,4 +1,7 @@
 # -*- coding: UTF8 -*-
+#Author: @n1nj4sec
+#Contributor(s): @bobsecq
+
 from pupylib.PupyModule import *
 
 __class_name__="GetPrivsModule"
@@ -9,8 +12,15 @@ class GetPrivsModule(PupyModule):
     dependencies=["psutil", "pupwinutils.security"]
     def init_argparse(self):
         self.arg_parser = PupyArgumentParser(prog="getprivs", description=self.__doc__)
+        self.arg_parser.add_argument('--get-debug', dest='getdebug', action='store_true', help="Try to get SeDebugPrivilege for the current process")
 
     def run(self, args):
-        self.client.conn.modules["pupwinutils.security"].EnablePrivilege("SeDebugPrivilege")
-        self.success("SeDebugPrivilege enabled !")
+        if args.getdebug == True:
+            self.client.conn.modules["pupwinutils.security"].EnablePrivilege("SeDebugPrivilege")
+            self.success("SeDebugPrivilege enabled !")
+        else:
+            privs = self.client.conn.modules["pupwinutils.security"].get_currents_privs()
+            self.success("Process privileges:")
+            for aPriv in privs:
+                 print " - "+str(aPriv)
 
--- pupy/packages/windows/all/pupwinutils/security.py
+++ pupy/packages/windows/all/pupwinutils/security.py
@@ -1,8 +1,12 @@
-#original code from https://github.com/joren485/PyWinPrivEsc/blob/master/RunAsSystem.py
+# -*- coding: UTF8 -*-
+#Author: ??? and original code from https://github.com/joren485/PyWinPrivEsc/blob/master/RunAsSystem.py
+#Contributor(s): @bobsecq
+
 import sys, os
 from ctypes import *
 import subprocess
 import psutil
+import ctypes
 
 LPVOID = c_void_p
 PVOID = LPVOID
@@ -36,11 +40,22 @@ TOKEN_ALL_ACCESS = (STANDARD_RIGHTS_REQUIRED | TOKEN_ASSIGN_PRIMARY |
 
 PROCESS_QUERY_INFORMATION = 0x0400
 
+class TOKEN_INFORMATION_CLASS:
+    #see http://msdn.microsoft.com/en-us/library/aa379626%28VS.85%29.aspx
+    TokenUser = 1
+    TokenGroups = 2
+    TokenPrivileges = 3
+
 class LUID(Structure):
     _fields_ = [
         ("LowPart",     DWORD),
         ("HighPart",    LONG),
     ]
+    def __eq__(self, other):
+        return (self.HighPart == other.HighPart and self.LowPart == other.LowPart)
+
+    def __ne__(self, other):
+        return not (self==other)
 
 class SID_AND_ATTRIBUTES(Structure):
     _fields_ = [
@@ -51,18 +66,63 @@ class SID_AND_ATTRIBUTES(Structure):
 class TOKEN_USER(Structure):
     _fields_ = [
         ("User", SID_AND_ATTRIBUTES),]
+        
+SE_PRIVILEGE_ENABLED_BY_DEFAULT = (0x00000001)
+SE_PRIVILEGE_ENABLED            = (0x00000002)
+SE_PRIVILEGE_REMOVED            = (0x00000004)
+SE_PRIVILEGE_USED_FOR_ACCESS    = (0x80000000)
+
+LookupPrivilegeName = ctypes.windll.advapi32.LookupPrivilegeNameW
+LookupPrivilegeName.argtypes = (
+    wintypes.LPWSTR, # lpSystemName
+    ctypes.POINTER(LUID), # lpLuid
+    wintypes.LPWSTR, # lpName
+    ctypes.POINTER(wintypes.DWORD), #cchName
+    )
+LookupPrivilegeName.restype = wintypes.BOOL
 
 class LUID_AND_ATTRIBUTES(Structure):
     _fields_ = [
         ("Luid",        LUID),
         ("Attributes",  DWORD),
     ]
+    def is_enabled(self):
+        return bool(self.Attributes & SE_PRIVILEGE_ENABLED)
+
+    def enable(self):
+        self.Attributes |= SE_PRIVILEGE_ENABLED
+
+    def get_name(self):
+        size = wintypes.DWORD(10240)
+        buf = ctypes.create_unicode_buffer(size.value)
+        res = LookupPrivilegeName(None, self.Luid, buf, size)
+        if res == 0: raise RuntimeError
+        return buf[:size.value]
+
+    def __str__(self):
+        res = self.get_name()
+        if self.is_enabled(): res += ' (enabled)'
+        return res
 
 class TOKEN_PRIVILEGES(Structure):
     _fields_ = [
         ("PrivilegeCount",  DWORD),
         ("Privileges",      LUID_AND_ATTRIBUTES),
     ]
+    
+class TOKEN_PRIVS(Structure):
+    _fields_ = [
+        ("PrivilegeCount",  DWORD),
+        ("Privileges",      LUID_AND_ATTRIBUTES*0),
+    ]
+    def get_array(self):
+        array_type = LUID_AND_ATTRIBUTES*self.PrivilegeCount
+        privileges = ctypes.cast(self.Privileges, ctypes.POINTER(array_type)).contents
+        return privileges
+
+    def __iter__(self):
+        return iter(self.get_array())
+
 
 class PROCESS_INFORMATION(Structure):
     _fields_ = [
@@ -190,6 +250,22 @@ def getProcessToken(pid):
     windll.advapi32.OpenProcessToken(hProcess, tokenprivs, byref(hToken))
     windll.kernel32.CloseHandle(hProcess)
     return hToken
+    
+def get_process_token():
+    """
+    Get the current process token
+    """
+    GetCurrentProcess = ctypes.windll.kernel32.GetCurrentProcess
+    GetCurrentProcess.restype = wintypes.HANDLE
+    OpenProcessToken = ctypes.windll.advapi32.OpenProcessToken
+    OpenProcessToken.argtypes = (wintypes.HANDLE, wintypes.DWORD, ctypes.POINTER(wintypes.HANDLE))
+    OpenProcessToken.restype = wintypes.BOOL
+    token = wintypes.HANDLE()
+    TOKEN_ALL_ACCESS = 0xf01ff
+    res = OpenProcessToken(GetCurrentProcess(), TOKEN_ALL_ACCESS, token)
+    if not res > 0:
+        raise RuntimeError("Couldn't get process token")
+    return token
 
 def gethTokenFromPid(pid):
     try:
@@ -366,3 +442,34 @@ def rev2self():
         pass
     global_ref=None
     print "\t[+] Running as: " + GetUserName()
+
+def get_currents_privs():
+    '''
+    Get all privileges associated with the current process.
+    '''
+    GetTokenInformation = ctypes.windll.advapi32.GetTokenInformation
+    GetTokenInformation.argtypes = [
+        wintypes.HANDLE, # TokenHandleTOKEN_PRIV
+        ctypes.c_uint, # TOKEN_INFORMATION_CLASS value
+        ctypes.c_void_p, # TokenInformation
+        wintypes.DWORD, # TokenInformationLength
+        ctypes.POINTER(wintypes.DWORD), # ReturnLength
+        ]
+    GetTokenInformation.restype = wintypes.BOOL
+    return_length = wintypes.DWORD()
+    params = [
+        get_process_token(),
+        TOKEN_INFORMATION_CLASS.TokenPrivileges,
+        None,
+        0,
+        return_length,
+    ]
+    res = GetTokenInformation(*params)
+    buffer = ctypes.create_string_buffer(return_length.value)
+    params[2] = buffer
+    params[3] = return_length.value
+    res = GetTokenInformation(*params)
+    assert res > 0, "Error in second GetTokenInformation (%d)" % res
+    privileges = ctypes.cast(buffer, ctypes.POINTER(TOKEN_PRIVS)).contents
+    return privileges
+    
