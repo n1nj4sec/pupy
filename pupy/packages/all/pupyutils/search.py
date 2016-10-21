f227d096e130d13b640dbfc36e35a3338e276154
Alessandro ZANNI <alessandro.zanni@bt.com>
2016-10-21 18:19:41 +0200

    changing search functions

--- pupy/packages/all/pupyutils/search.py
+++ pupy/packages/all/pupyutils/search.py
@@ -1,42 +1,75 @@
 #!/usr/bin/env python
-# -*- coding: UTF8 -*-
+# -*- coding: utf-8 -*-
+from scandir import scandir, walk
+import time
 import os
-import os.path
 import re
+import sys
 
-def search_file(path, search_strings):
-    buf=b""
-    line_nb=0
-    try:
-        with open(path, 'rb') as f:
-            for line in f:
-                line=line.lower()
-                for s in search_strings:
-                    start=0
-                    while True:
-                        i=line.find(s.lower(), start)
-                        if i==-1:
-                            break
-                        start=i+1
-                        yield (line_nb+1, line[i:i+50].strip())
-                line_nb+=1
-    except Exception:
-        pass
+class Search():
+    def __init__(self, files_extensions='', max_size=20000000, check_content=False, root_path='.', search_str=[]):
+        # By default max size is 20 Mo
+        self.max_size = max_size
+        self.files_extensions = files_extensions
+        self.check_content = check_content
+        if root_path == '.':
+            self.root_path = os.getcwd()
+        else:
+            self.root_path = root_path
+        self.search_str = search_str
 
+    def search_string(self, path, search_str):
+        buffer_size = 4096
+        buffer = None
+        try:
+            with open(path, 'rb') as f:    
+                while True:
+                    buffer = f.read(buffer_size)
+                    if buffer:
+                        for string in search_str:
+                            # no case sensitive on regex
+                            indexes = [m.start() for m in re.finditer(string, buffer, flags=re.IGNORECASE)]
+                            for i in indexes:
+                                # return the entire line
+                                yield buffer[i:].strip().split('\n')[0]
+                    else:
+                        break
+        except:
+            pass
 
-def search_path(path, search_strings, files_extensions=None, max_size=None):
-    """ search recursively for a string in all files in the path """
-    if not files_extensions:
-        files_extensions=None
-    if files_extensions is not None:
-        files_extensions=tuple(files_extensions)
-    for root, dirs, files in os.walk(path):
-        for f in files:
-            if files_extensions is None or f.lower().endswith(files_extensions):
-                if max_size is None or os.path.getsize(os.path.join(root,f))<max_size:
-                    for res in search_file(os.path.join(root,f),search_strings):
-                        yield (os.path.join(root,f), res[0], res[1])
-    
-if __name__=="__main__":
-    import sys
-    search_path(sys.argv[1],[sys.argv[2]])
+    def scanwalk(self, path, followlinks=False):
+        ''' lists of DirEntries instead of lists of strings '''
+        dirs, nondirs = [], []
+        try:
+            for entry in scandir(path):
+                if entry.is_dir(follow_symlinks=followlinks):
+                    dirs.append(entry)
+                else:
+                    if self.max_size > entry.stat(follow_symlinks=False).st_size:
+                        if entry.name.endswith(self.files_extensions):
+                            nondirs.append(entry)
+            yield path, dirs, nondirs
+        # try / except used for permission denied 
+        except:
+            pass
+        
+        for dir in dirs:
+            for res in self.scanwalk(dir.path, followlinks=followlinks):
+                yield res
+
+    def run(self):
+        for root, dirs, files in self.scanwalk(self.root_path):
+            for f in files:
+                # such as find command
+                for s in self.search_str:
+                    if f.name.lower().find(s) != -1:
+                        yield 'File: %s\n\n' % os.path.join(root, f.name)
+
+                # such as grep command
+                if self.check_content:
+                    for res in self.search_string(os.path.join(root, f.name), self.search_str):
+                        try:
+                            res = res.encode('utf-8')
+                            yield 'File: %s > %s\n\n' % (os.path.join(root, f.name), res)
+                        except:
+                            pass
