8a3dee31caabcfaaa36af38162de27b63a9b1424
Oleksii Shevchuk <alxchk@gmail.com>
2016-12-02 15:48:15 +0200

    Add new API to select paths either in workdir or at home folders

--- pupy/pupylib/PupyConfig.py
+++ pupy/pupylib/PupyConfig.py
@@ -4,21 +4,49 @@ try:
 except ImportError:
     from configparser import ConfigParser, Error
 
-from os import path
+from os import path, makedirs
 from netaddr import IPAddress
 
 class PupyConfig(ConfigParser):
     def __init__(self, config='pupy.conf'):
         self.root = path.abspath(path.join(path.dirname(__file__), '..'))
+        self.user_root = path.expanduser(path.join('~', '.config', 'pupy'))
         self.files = [
             path.join(self.root, config+'.default'),
             path.join(self.root, config),
-            path.expandvars(path.expanduser(path.join('~', '.config', 'pupy', config))),
+            path.join(self.user_root, config),
             config
         ]
 
         ConfigParser.__init__(self)
         self.read(self.files)
+
+    def get_folder(self, folder='data', substitutions={}, create=True):
+        prefer_workdir = self.getboolean('paths', 'prefer_workdir')
+        from_config = self.get('paths', folder)
+
+        retfolder = ''
+        if from_config:
+            retfolder = from_config
+        elif path.isabs(folder):
+            retfolder = folder
+        elif prefer_workdir:
+            retfolder = folder
+        else:
+            retfolder = path.join(self.user_root, folder)
+
+        for key, value in substitutions.iteritems():
+            retfolder = retfolder.replace(key, value)
+
+        if path.isdir(retfolder):
+            return retfolder
+        elif path.exists(retfolder):
+            raise ValueError('{} is not a folder'.format(retfolder))
+        elif create:
+            makedirs(retfolder)
+            return retfolder
+        else:
+            return retfolder
 
     def get(self, *args, **kwargs):
         try:
