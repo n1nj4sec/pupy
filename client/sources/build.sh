ecdedfe5ddb3350f3320ef53f6d21bf06f913410
Oleksii Shevchuk <alxchk@gmail.com>
2016-12-02 16:00:25 +0200

    Schedule debug build in build.sh (windows)

--- client/sources/build.sh
+++ client/sources/build.sh
@@ -3,5 +3,9 @@
 ./buildenv.sh
 make -f Makefile ARCH=win32 clean
 make -f Makefile ARCH=win32
+make -f Makefile DEBUG=1 ARCH=win32 clean
+make -f Makefile ARCH=win32
 make -f Makefile ARCH=win64 clean
+make -f Makefile ARCH=win64
+make -f Makefile DEBUG=1 ARCH=win64 clean
 make -f Makefile ARCH=win64
