470352143280388961606399054f651b52b98fdb
AlessandroZ <zanni.alessandro@gmail.com>
2017-02-09 16:17:15 +0100

    lock screen for mac os

--- pupy/modules/lock_screen.py
+++ pupy/modules/lock_screen.py
@@ -1,9 +1,10 @@
 # -*- coding: UTF8 -*-
 from pupylib.PupyModule import *
+import subprocess
 
 __class_name__="PupyMod"
 
-@config(compat="windows", cat="manage", tags=["lock", "screen", "session"])
+@config(compat=["windows", "darwin"], cat="manage", tags=["lock", "screen", "session"])
 class PupyMod(PupyModule):
     """ Lock the session """
 
@@ -11,8 +12,13 @@ class PupyMod(PupyModule):
         self.arg_parser = PupyArgumentParser(prog="lock_screen", description=self.__doc__)
 
     def run(self, args):
-        if self.client.conn.modules['ctypes'].windll.user32.LockWorkStation():
+        ok = False
+        if self.client.is_windows():
+            ok = self.client.conn.modules['ctypes'].windll.user32.LockWorkStation()
+        elif self.client.is_darwin():
+            ok = self.client.conn.modules.subprocess.Popen('/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend', stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
+
+        if ok:
             self.success("windows locked")
         else:
             self.error("couldn't lock the screen")
-
