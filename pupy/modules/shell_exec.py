1c803bb5c119e569ed985a70dddbc309be8b81a9
AlessandroZ <zanni.alessandro@gmail.com>
2017-02-09 15:32:54 +0100

    hide functionality available only for windows hosts

--- pupy/modules/shell_exec.py
+++ pupy/modules/shell_exec.py
@@ -10,12 +10,14 @@ __class_name__="ShellExec"
 @config(cat="admin")
 class ShellExec(PupyModule):
     """ execute shell commands on a remote system """
+    
     def init_argparse(self):
         self.arg_parser = PupyArgumentParser(prog='shell_exec', description=self.__doc__)
         self.arg_parser.add_argument('-s', '--shell', help="default to /bin/sh on linux or cmd.exe on windows")
         self.arg_parser.add_argument('argument', help='use unix like syntax and put simple quotes if there is multiple arguments')
         self.arg_parser.add_argument('-H', '--hide', action='store_true', help='launch process on background (only for windows)')
         self.arg_parser.add_argument('-log', help='Save log to file (when not background process)')
+    
     def run(self, args):
         if not args.hide:
             log = shell_exec(self.client, args.argument, shell=args.shell)
@@ -23,7 +25,7 @@ class ShellExec(PupyModule):
             if args.log:
                 with open(args.log, 'wb') as output:
                     output.write(log)
-        else:
+        elif args.hide and self.client.is_windows():
             try:
                 self.client.load_package("psutil")
                 self.client.load_package("pupwinutils.processes")
@@ -32,3 +34,5 @@ class ShellExec(PupyModule):
                 self.success("Process created with pid %s" % p.pid)
             except Exception, e:
                 self.error("Error creating the process: %s" % e)
+        else:
+            self.error('--hide option works only for Windows hosts')
