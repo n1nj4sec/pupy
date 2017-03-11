32b1fbca497328dc5da1c1cc41e02016622882fb
Oleksii Shevchuk <alxchk@gmail.com>
2017-03-11 12:12:23 +0200

    More properly search for local/external address

--- pupy/pupylib/utils/network.py
+++ pupy/pupylib/utils/network.py
@@ -3,29 +3,54 @@ import netifaces
 import urllib2
 import netaddr
 
-def get_local_ip(iface = None):
+LOCAL_IP = None
+
+def get_local_ip(iface=None, cache=True, external=False):
     '''
     Returns local ip address (no 127.0.0.1) or None
     '''
+
+    global LOCAL_IP
+
+    if LOCAL_IP and cache:
+       return LOCAL_IP
+
     try:
-        if iface != None:
+        ifaces = []
+
+        if iface:
             ifaces = [iface]
-        else:
+
+        elif external:
             proxy = urllib2.ProxyHandler()
             opener = urllib2.build_opener(proxy)
             opener.addheaders = [('User-agent', 'curl/7.50.0')]
             try:
                 response = opener.open('http://ifconfig.co', timeout=5)
                 if response.code == 200:
-                    return str(netaddr.IPAddress(response.read()))
+                    LOCAL_IP = str(netaddr.IPAddress(response.read()))
+                    return LOCAL_IP
             except:
                 pass
 
+        if not ifaces:
+            ifaces = [
+                x for x in netifaces.interfaces() if not x.startswith(('lo', 'docker'))
+            ]
+
         for anInt in ifaces:
-            addr = netifaces.ifaddresses(anInt)[netifaces.AF_INET][0]['addr']
-            if addr != "127.0.0.1":
+            addresses = netifaces.ifaddresses(anInt)
+            if not netifaces.AF_INET in addresses:
+                continue
+
+            ipaddr = addresses[netifaces.AF_INET][0]
+            if not 'addr' in ipaddr:
+                continue
+
+            addr = ipaddr['addr']
+            if addr not in ('127.0.0.1', '0.0.0.0'):
                 return addr
 
-        return None
-    except Exception:
+    except Exception, e:
+        print "exception: {}".format(e)
         return None
