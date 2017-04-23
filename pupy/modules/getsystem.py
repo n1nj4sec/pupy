645cf0812316ea045e2537548475558f17db94b9
n1nj4sec <contact@n1nj4.eu>
2017-04-23 14:26:25 +0200

    add --prog arg to getsystem

--- pupy/modules/getsystem.py
+++ pupy/modules/getsystem.py
@@ -10,9 +10,10 @@ class GetSystem(PupyModule):
     dependencies=["pupwinutils.security"]
     def init_argparse(self):
         self.arg_parser = PupyArgumentParser(prog="getsystem", description=self.__doc__)
+        self.arg_parser.add_argument("--prog", default="cmd.exe", help="Change the default process to create/inject into")
 
     def run(self, args):
         with redirected_stdo(self):
-            proc_pid=self.client.conn.modules["pupwinutils.security"].getsystem()
+            proc_pid=self.client.conn.modules["pupwinutils.security"].getsystem(prog=args.prog)
         migrate(self, proc_pid)
         self.success("got system !")
