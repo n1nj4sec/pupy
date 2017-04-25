61dc5fc6f4cc40b287f380ed270b5dd3da9888f6
Oleksii Shevchuk <ol.shevchuk@samsung.com>
2017-04-25 10:47:05 +0300

    sys.flags.optimize can't be set in runtime

--- client/gen_python_bootloader.py
+++ client/gen_python_bootloader.py
@@ -63,7 +63,7 @@ if __name__=="__main__":
         remove_stdout if not args.debug else 'print "DEBUG"\n',
         'import sys; sys.path=[]; sys.path_hooks=[]; sys.meta_path=[];' + (
             'sys.argv = [];' if not args.pass_argv else ''
-        ) + 'sys.prefix = ""; sys.flags.optimize=1;\n',
+        ) + 'sys.prefix = "";\n',
         pupyload.format('pupyimporter', repr(pupyimporter)),
         'import pupyimporter\n'
         'pupyimporter.install({})\n'.format(args.debug),
