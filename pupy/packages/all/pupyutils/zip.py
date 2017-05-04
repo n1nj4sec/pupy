6e3f2b5b200bddd96eb63dda96000ed5b625f547
Oleksii Shevchuk <ol.shevchuk@samsung.com>
2017-05-04 13:06:26 +0300

    Try to covert path to unicode (pupyutils.zip)

--- pupy/packages/all/pupyutils/zip.py
+++ pupy/packages/all/pupyutils/zip.py
@@ -1,62 +1,77 @@
+# -*- coding: utf-8 -*-
+
 import os
 import zipfile
 
+def try_unicode(path):
+    if type(path) != unicode:
+        try:
+            return path.decode('utf-8')
+        except UnicodeDecodeError:
+            pass
+
+    return path
+
 def zip(src, dst):
+    src = try_unicode(src)
+    dst = try_unicode(dst)
 
-	if not os.path.exists(src):
-		print "[-] The file \"%s\" does not exists" % src
-		return
+    if not os.path.exists(src):
+        print "[-] The file \"%s\" does not exists" % src
+        return
 
-	isDir = False
-	if os.path.isdir(src):
-		isDir = True
+    isDir = False
+    if os.path.isdir(src):
+        isDir = True
 
-	if not dst:
-		if isDir:
-			d = src.split(os.sep)
-			dst = d[len(d)-1] + '.zip'
-		else:
-			dst = src + '.zip'
+    if not dst:
+        if isDir:
+            d = src.split(os.sep)
+            dst = d[len(d)-1] + '.zip'
+        else:
+            dst = src + '.zip'
 
-	# To not overwrite an existing file
-	if os.path.exists(dst):
-		print "[-] The destination file \"%s\" already exists" % dst
-		return
+    # To not overwrite an existing file
+    if os.path.exists(dst):
+        print "[-] The destination file \"%s\" already exists" % dst
+        return
 
-	# Zip process
-	zf = zipfile.ZipFile(dst, "w", zipfile.ZIP_DEFLATED)
-	if isDir:
-		abs_src = os.path.abspath(src)
-		for dirname, subdirs, files in os.walk(src):
-			for filename in files:
-				absname = os.path.abspath(os.path.join(dirname, filename))
-				arcname = absname[len(abs_src) + 1:]
-				zf.write(absname, arcname)
-	else:
-		zf.write(src)
+    # Zip process
+    zf = zipfile.ZipFile(dst, "w", zipfile.ZIP_DEFLATED)
+    if isDir:
+        abs_src = os.path.abspath(src)
+        for dirname, subdirs, files in os.walk(src):
+            for filename in files:
+                absname = os.path.abspath(os.path.join(dirname, filename))
+                arcname = absname[len(abs_src) + 1:]
+                zf.write(absname, arcname)
+    else:
+        zf.write(src)
 
-	print "[+] File zipped correctly: \"%s\"" % dst
-	zf.close()
+    print "[+] File zipped correctly: \"%s\"" % dst
+    zf.close()
 
 
 def unzip(src, dst):
-	
-	if not os.path.exists(src):
-		print "[-] The file \"%s\" does not exists" % src
-		return
+    src = try_unicode(src)
+    dst = try_unicode(dst)
 
-	if not dst:
-		d = src.split(os.sep)
-		dst = d[len(d)-1].replace('.zip', '')
+    if not os.path.exists(src):
+        print "[-] The file \"%s\" does not exists" % src
+        return
 
-	# To not overwrite an existing file
-	if os.path.exists(dst):
-		print "[-] The destination file \"%s\" already exists" % dst
-		return
+    if not dst:
+        d = src.split(os.sep)
+        dst = d[len(d)-1].replace('.zip', '')
 
-	if zipfile.is_zipfile(src):
-		with zipfile.ZipFile(src, "r") as z:
-			z.extractall(dst)
-		print "[+] File unzipped correctly: \"%s\"" % dst
-	else:
-		print '[-] The zipped file does not have a valid zip format: \"%s\"'
+    # To not overwrite an existing file
+    if os.path.exists(dst):
+        print "[-] The destination file \"%s\" already exists" % dst
+        return
+
+    if zipfile.is_zipfile(src):
+        with zipfile.ZipFile(src, "r") as z:
+            z.extractall(dst)
+        print "[+] File unzipped correctly: \"%s\"" % dst
+    else:
+        print '[-] The zipped file does not have a valid zip format: \"%s\"'
