25e0dc3b06afa8c053704ea26e3b2e355a9e575d
Oleksii Shevchuk <alxchk@gmail.com>
2017-07-02 18:56:09 +0300

    Build windows/uncompressed templates

--- client/sources/build.sh
+++ client/sources/build.sh
@@ -5,7 +5,15 @@ make -f Makefile ARCH=win32 clean
 make -f Makefile ARCH=win32
 make -f Makefile DEBUG=1 ARCH=win32 clean
 make -f Makefile DEBUG=1 ARCH=win32
+make -f Makefile ARCH=win32 UNCOMPRESSED=1 clean
+make -f Makefile ARCH=win32 UNCOMPRESSED=1 
+make -f Makefile DEBUG=1 ARCH=win32 UNCOMPRESSED=1 clean
+make -f Makefile DEBUG=1 ARCH=win32 UNCOMPRESSED=1 
 make -f Makefile ARCH=win64 clean
 make -f Makefile ARCH=win64
 make -f Makefile DEBUG=1 ARCH=win64 clean
 make -f Makefile DEBUG=1 ARCH=win64
+make -f Makefile ARCH=win64 UNCOMPRESSED=1 clean
+make -f Makefile ARCH=win64 UNCOMPRESSED=1
+make -f Makefile DEBUG=1 ARCH=win64 UNCOMPRESSED=1 clean
+make -f Makefile DEBUG=1 ARCH=win64 UNCOMPRESSED=1
