ffbf2ada7a2d0d53c7068680a1ee715957845a61
quentinhardy <quentin.hardy@protonmail.com>
2017-10-23 09:20:16 -0400

    forward module visible in list_modules

--- pupy/modules/forward.py
+++ pupy/modules/forward.py
@@ -22,7 +22,6 @@ __class_name__ = 'Forward'
 @config(cat='network', tags=['forward', 'pivot'])
 class Forward(PupyModule):
     ''' Local/remote port forwarding and SOCKS proxy '''
-    is_module = False
     dependencies = {
         'all': [ 'pyuv', 'pyuvproxy' ],
     }
