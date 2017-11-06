bc1db50ea60697077fcf9a912456c7d51590a8bc
ire4ever1190 <Jakeleahy@gmail.com>
2017-11-06 14:46:56 +1100

    Create init.sh
    add init script to make it easier to install

---
+++ init.sh
@@ -0,0 +1,5 @@
+git submodule init
+git submodule update
+pip install -r pupy/requirements.txt
+wget https://github.com/n1nj4sec/pupy/releases/download/latest/payload_templates.txz
+tar xvf payload_templates.txz && mv payload_templates/* pupy/payload_templates/ && rm payload_templates.txz && rm -r payload_templates
