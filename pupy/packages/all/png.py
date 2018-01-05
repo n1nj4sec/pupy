54da21af5b2e0bb859ba5ed803c0b62fe61f6719
Oleksii Shevchuk <alxchk@gmail.com>
2018-01-05 22:10:15 +0200

    Fix png module

--- pupy/packages/all/png.py
+++ pupy/packages/all/png.py
@@ -3,7 +3,7 @@
 import struct
 from zlib import compress, crc32
 
-def bmp_to_png(data, width, height, compress=9):
+def bmp_to_png(data, width, height, compression=9):
     # From MSS
     line = width * 3
     png_filter = struct.pack('>B', 0)
@@ -19,7 +19,7 @@ def bmp_to_png(data, width, height, compress=9):
     ihdr[0] = struct.pack('>I', len(ihdr[2]))
 
     # Data: size, marker, data, CRC32
-    idat = [b'', b'IDAT', compress(scanlines, compress), b'']
+    idat = [b'', b'IDAT', compress(scanlines, compression), b'']
     idat[3] = struct.pack('>I', crc32(b''.join(idat[1:3])) & 0xffffffff)
     idat[0] = struct.pack('>I', len(idat[2]))
 
