e7fc9fee07c495b2b00505627daac827aaec059e
Oleksii Shevchuk <alxchk@gmail.com>
2018-02-10 08:27:38 +0200

    Add module to retrieve SSL certificate

---
+++ pupy/modules/x509.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from pupylib.PupyModule import *
+from M2Crypto.X509 import load_cert_string
+
+__class_name__='x509'
+
+@config(cat='admin')
+class x509(PupyModule):
+    ''' Fetch certificate from server '''
+
+    def init_argparse(self):
+        self.arg_parser = PupyArgumentParser(prog='x509', description=self.__doc__)
+        self.arg_parser.add_argument('host', help='Address')
+        self.arg_parser.add_argument('port', type=int, help='Port')
+        self.arg_parser.add_argument('-t', '--text', action='store_true', default=False,
+                                     help='Convert to text')
+
+
+    def run(self, args):
+        ssl = self.client.conn.modules['ssl']
+        cert = ssl.get_server_certificate((args.host, args.port))
+        if args.text:
+            cert = load_cert_string(cert).as_text()
+        self.log(cert)
