0d499c8d3193247f5bb96854a5f6af1771b16804
Oleksii Shevchuk <alxchk@gmail.com>
2018-02-16 14:47:38 +0200

    There is no need to encode with base64

--- pupy/pupylib/utils/obfuscate.py
+++ pupy/pupylib/utils/obfuscate.py
@@ -3,8 +3,8 @@
 # Copyright (c) 2015, Nicolas VERDIER (contact@n1nj4.eu)
 # Pupy is under the BSD 3-Clause license. see the LICENSE file at the root of the project for the detailed licence terms
 
-import marshal, zlib, base64
+import marshal, zlib
 
 def compress_encode_obfs(code):
-    return "import zlib,base64,marshal;exec marshal.loads(zlib.decompress(base64.b64decode(%s)))"%repr(
-        base64.b64encode(zlib.compress(marshal.dumps(compile(code, '', 'exec')), 9)))
+    return "import zlib,marshal;exec marshal.loads(zlib.decompress(%s))"%repr(
+        zlib.compress(marshal.dumps(compile(code, '', 'exec')), 9))
