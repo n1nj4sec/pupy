97c6725348fe45c693c94a1c69a61c668a347114
Oleksii Shevchuk <alxchk@gmail.com>
2018-02-27 13:11:34 +0200

    Add shortcut to parse remote local x509 certs

--- pupy/modules/x509.py
+++ pupy/modules/x509.py
@@ -8,19 +8,27 @@ __class_name__='x509'
 class x509(PupyModule):
     ''' Fetch certificate from server '''
 
+    dependencies = [ 'pupyutils.basic_cmds' ]
+
     def init_argparse(self):
         self.arg_parser = PupyArgumentParser(prog='x509', description=self.__doc__)
-        self.arg_parser.add_argument('host', help='Address')
-        self.arg_parser.add_argument('port', type=int, help='Port')
-        self.arg_parser.add_argument('-t', '--text', action='store_true', default=False,
-                                     help='Convert to text')
+        self.arg_parser.add_argument('host', help='Address or path')
+        self.arg_parser.add_argument('port', type=int, default=443, nargs='?', help='Port')
+        self.arg_parser.add_argument('-F', '--file', action='store_true', default=False,
+                                     help='Force treat host as file path')
+        self.arg_parser.add_argument('-R', '--raw', action='store_true', default=False,
+                                     help='Do not convert to text')
 
 
     def run(self, args):
-        get_server_certificate = self.client.remote('ssl', 'get_server_certificate')
+        if args.file or '/' in args.host or '\\' in args.host:
+            cat = self.client.remote('pupyutils.basic_cmds', 'cat', False)
+            cert = cat(args.host)
+        else:
+            get_server_certificate = self.client.remote('ssl', 'get_server_certificate')
+            cert = get_server_certificate((args.host, args.port))
 
-        cert = get_server_certificate((args.host, args.port))
-        if args.text:
+        if not args.raw:
             cert = load_cert_string(cert).as_text()
 
         self.log(cert)
