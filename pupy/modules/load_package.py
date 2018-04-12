c2df2c945706b39afa53844542cf8d32177d3188
Oleksii Shevchuk <alxchk@gmail.com>
2018-04-12 13:29:37 +0300

    Add option to invalidate package manually

--- pupy/modules/load_package.py
+++ pupy/modules/load_package.py
@@ -29,6 +29,7 @@ class LoadPackageModule(PupyModule):
     def init_argparse(cls):
         cls.arg_parser = PupyArgumentParser(prog="load_package", description=cls.__doc__)
         cls.arg_parser.add_argument('-f', '--force', action='store_true', help='force package to reload even if it has already been loaded')
+        cls.arg_parser.add_argument('-r', '--remove', action='store_true', help='remove (invalidate) module')
         cls.arg_parser.add_argument('-d', '--dll', action='store_true', help='load a dll instead')
         cls.arg_parser.add_argument('package', completer=package_completer, help='package name (example: psutil, scapy, ...)')
 
@@ -36,11 +37,15 @@ class LoadPackageModule(PupyModule):
     def run(self, args):
         if args.dll:
             if self.client.load_dll(args.package):
-                self.success("dll loaded !")
+                self.success('dll loaded')
             else:
-                self.error("the dll was already loaded")
+                self.error('the dll was already loaded')
+        elif args.remove:
+            invalidated = self.client.invalidate_packages([args.package])
+            if invalidated:
+                self.success('package invalidated')
         else:
             if self.client.load_package(args.package, force=args.force):
-                self.success("package loaded !")
+                self.success('package loaded')
             else:
-                self.error("package is already loaded !")
+                self.warning('package is already loaded')
