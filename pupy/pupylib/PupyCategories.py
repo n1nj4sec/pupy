6006785532ff595dd7fae136f481b10be9e5b79f
Oleksii Shevchuk <alxchk@gmail.com>
2018-04-14 20:51:35 +0300

    Remove unused decorators

--- pupy/pupylib/PupyCategories.py
+++ pupy/pupylib/PupyCategories.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-# -*- coding: UTF8 -*-
+# -*- coding: utf-8 -*-
 # Copyright (c) 2015, Nicolas VERDIER (contact@n1nj4.eu)
 # Pupy is under the BSD 3-Clause license. see the LICENSE file at the root of the project for the detailed licence terms
 
 from . import conf
+
 import logging
 
 class PupyCategories(object):
