4072a77284a1193dc9db48291e62cfa7e1a031fb
Strazzom <user@fedora-23-dvm>
2018-09-29 16:12:44 -0700

    Added modified files from old fork

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Apt update and installs
+apt update
+apt install python-pip curl -y
+
+# Install Docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+
+if [ -f /etc/apt/sources.list.d/docker.list ]; then
+    echo "Apt source entry exists, skipping."
+else
+    echo 'deb https://download.docker.com/linux/debian stretch stable' > /etc/apt/sources.list.d/docker.list
+fi
+
+apt update
+apt-get install docker-ce -y
+systemctl start docker
+systemctl enable docker
+
+# Install Docker Compose
+pip install docker-compose
+
+# Pull dependencies from github
+git submodule update --init --recursive
+
+# Download latest compiled payload templates
+wget https://github.com/n1nj4sec/pupy/releases/download/latest/payload_templates.txz
+tar xvf payload_templates.txz && mv payload_templates/* pupy/payload_templates/ && rm payload_templates.txz && rm -r payload_templates
+
+# Pull docker container
+docker pull alxchk/pupy:unstable
