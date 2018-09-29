3517a3c984f590209abd09e5f32ec6871f61b2a1
Strazzom <user@fedora-23-dvm>
2018-09-29 16:49:12 -0700

    Revert "modified install.sh to build image instead of pulling it"
    This reverts commit c1917fa43a659eaaeec6f7f870266110c1671c38.

--- install.sh
+++ install.sh
@@ -40,5 +40,5 @@ git submodule update --init --recursive
 wget https://github.com/n1nj4sec/pupy/releases/download/latest/payload_templates.txz
 tar xvf payload_templates.txz && mv payload_templates/* pupy/payload_templates/ && rm payload_templates.txz && rm -r payload_templates
 
-# Build docker container
-docker build -t alxchk/pupy:nodeps -f ./pupy/Dockerfile.compose pupy
+# Pull docker container
+docker pull alxchk/pupy:unstable
