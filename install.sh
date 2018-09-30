26f816eab2a684ca7827e38ed061df8fedf320ca
Strazzom <user@fedora-23-dvm>
2018-09-30 14:39:54 -0700

    changed image name from nodeps to base

--- install.sh
+++ install.sh
@@ -41,4 +41,4 @@ wget https://github.com/n1nj4sec/pupy/releases/download/latest/payload_templates
 tar xvf payload_templates.txz && mv payload_templates/* pupy/payload_templates/ && rm payload_templates.txz && rm -r payload_templates
 
 # Build docker container
-docker build -t alxchk/pupy:nodeps -f ./pupy/Dockerfile.compose pupy
+docker build -t alxchk/pupy:base -f ./pupy/Dockerfile.compose pupy
