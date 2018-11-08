f2c02b4ee235f144a46dbef9d1e30f3fadb4e722
Oleksii Shevchuk <alxchk@gmail.com>
2018-11-08 09:59:36 +0200

    dnscnc: reask status if something went wrong during transfer

--- pupy/network/lib/picocmd/server.py
+++ pupy/network/lib/picocmd/server.py
@@ -775,6 +775,10 @@ class DnsCommandServerHandler(BaseResolver):
                 session._user_active_reported = session.system_status['idle']
 
             commands = session.commands
+
+            if not session.system_info:
+                commands.append(Poll())
+
             return commands
 
         elif isinstance(command, SystemInfo) and not session:
