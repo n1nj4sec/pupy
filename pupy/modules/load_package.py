fabb684c80ab2e8e69a48a16e2f9b07702819c92
AlessandroZ <zanni.alessandro@gmail.com>
2018-11-16 14:47:15 +0100

    fix load_package completer

--- pupy/modules/load_package.py
+++ pupy/modules/load_package.py
@@ -3,20 +3,24 @@
 from pupylib.PupyModule import config, PupyModule, PupyArgumentParser
 from pupylib.PupyCompleter import package_completer
 
-__class_name__="LoadPackageModule"
+__class_name__ = "LoadPackageModule"
+
 
 @config(cat="manage", compat="all")
 class LoadPackageModule(PupyModule):
-    """ Load a python package onto a remote client. Packages files must be placed in one of the pupy/packages/<os>/<arch>/ repository """
+    """
+    Load a python package onto a remote client. Packages files must be placed in one of the pupy/packages/<os>/<arch>/ repository
+    """
 
     @classmethod
     def init_argparse(cls):
         cls.arg_parser = PupyArgumentParser(prog="load_package", description=cls.__doc__)
-        cls.arg_parser.add_argument('-f', '--force', action='store_true', help='force package to reload even if it has already been loaded')
+        cls.arg_parser.add_argument('-f', '--force', action='store_true', help='force package to reload even '
+                                                                               'if it has already been loaded')
         cls.arg_parser.add_argument('-r', '--remove', action='store_true', help='remove (invalidate) module')
         cls.arg_parser.add_argument('-d', '--dll', action='store_true', help='load a dll instead')
-        cls.arg_parser.add_argument('package', completer=package_completer, help='package name (example: psutil, scapy, ...)')
-
+        cls.arg_parser.add_argument('package', completer=package_completer, help='package name '
+                                                                                 '(example: psutil, scapy, ...)')
 
     def run(self, args):
         if args.dll:
