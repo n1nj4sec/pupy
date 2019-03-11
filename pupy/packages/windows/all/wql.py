8fc49089eb88682476c4d92aad0ffdb92b28929f
Oleksii Shevchuk <alxchk@gmail.com>
2019-03-11 20:08:18 +0200

    wmi/wql: add module to query WMI properties

--- pupy/packages/windows/all/wql.py
+++ pupy/packages/windows/all/wql.py
@@ -10,4 +10,36 @@ def execute(query):
         pythoncom.CoInitialize()
         client = wmi.WMI()
 
-    return tuple(client.query(query))
+    return client.query(query)
+
+def execute_final(query):
+    response = execute(query)
+
+    columns = set()
+    keys = set()
+    result = []
+    order = None
+
+    for item in response:
+        keys.update(item.keys)
+        columns.update(item.properties.keys())
+
+        result.append(
+            tuple((column, getattr(item, column)) for column in item.properties)
+        )
+
+    _query = query.lower()
+    try:
+        idx_select = _query.index('select') + 7
+        idx_from = _query.index('from')
+
+        fields = query[idx_select:idx_from]
+        if '*' not in fields:
+            maybe_columns = tuple(x.strip() for x in fields.split(','))
+            if all(column in columns for column in maybe_columns):
+                columns = maybe_columns
+
+    except ValueError:
+        pass
+
+    return tuple(keys), tuple(columns), tuple(result)
