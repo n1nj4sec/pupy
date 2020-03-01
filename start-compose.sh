197701ef7b112a6c288bd1bd76f309a126e976b2
Oleksii Shevchuk <alxchk@gmail.com>
2020-03-01 08:34:21 +0200

    docker-compose: do not use UID/GID variables (may be protected)

--- start-compose.sh
+++ start-compose.sh
@@ -3,7 +3,8 @@ THIS=$0
 PUPY=`dirname "$0"`
 PUPY=`readlink -f ${PUPY}`/pupy
 WORKDIR=${1:-$HOME/pupy}
-GID=${GID:-`id -g`}
+CUID=${UID:-`id -u`}
+CGID=${GID:-`id -g`}
 TAG=${TAG:-"unstable"}
 
 set -e
@@ -12,14 +13,10 @@ if [ ! -d ${WORKDIR} ]; then
     mkdir -p ${WORKDIR}
 fi
 
-if [ ! -z "${UID}" ]; then
-    UID=`id -u`
-fi
-
-echo "[+] Workdir: ${WORKDIR} [UID=${UID} GID=${GID}]"
+echo "[+] Workdir: ${WORKDIR} [UID=${CUID} GID=${CGID}]"
 
 cd ${PUPY}
 
-export PUPY UID GID WORKDIR TAG
+export PUPY CUID CGID WORKDIR TAG
 docker-compose -f conf/docker-compose.yml up -d
 exec docker attach pupy
