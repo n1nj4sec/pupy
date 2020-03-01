3d49c426c4b76d0b24bd453d4bfa06942985dcdc
Oleksii Shevchuk <alxchk@gmail.com>
2020-03-01 08:13:31 +0200

    pupygen: Fix imports

--- pupy/pupygen.py
+++ pupy/pupygen.py
@@ -19,7 +19,7 @@ import subprocess
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 
 if __name__ == '__main__':
-    sys.path.insert(0, os.path.join(ROOT, 'pupy', 'library_patches'))
+    sys.path.insert(0, os.path.join(ROOT, 'library_patches'))
 
 import marshal
 import base64
