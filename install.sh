8497205da0249d7647ababb3b47b1df8ac3076bf
Not Available <56367254+mrdeepmind@users.noreply.github.com>
2021-09-06 17:56:40 +0300

    added pip3 install poster3
    pip3 install poster3

--- install.sh
+++ install.sh
@@ -27,6 +27,7 @@ apt-get install \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io docker-compose
+pip3 install poster3
 
 #Enable docker services
 systemctl enable docker.service
